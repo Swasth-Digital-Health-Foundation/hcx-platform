381626efbb751bea945ebdeae05893c0ee85349d
Shiva Rakshith <rakshiths@sanketika.in>
2021-10-29 12:15:02 +0530

    Update CoverageEligibilityController.java

--- hcx-apis/src/main/java/org/swasth/hcx/controllers/v1/CoverageEligibilityController.java
+++ hcx-apis/src/main/java/org/swasth/hcx/controllers/v1/CoverageEligibilityController.java
@@ -1,12 +1,13 @@
 package org.swasth.hcx.controllers.v1;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;;
-import org.swasth.hcx.controllers.BaseController;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.swasth.hcx.controllers.BaseController;
 import org.swasth.hcx.pojos.Response;
 import org.swasth.hcx.utils.ApiId;
+
 import java.util.Map;
 
 @RestController()
@@ -17,6 +18,8 @@ public class CoverageEligibilityController extends BaseController {
     public ResponseEntity<Object> checkCoverageEligibility(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
         Response response = getResponse(ApiId.COVERAGE_ELIGIBILITY_CHECK);
         try {
+            validateHeaders(header);
+            validatePayload(requestBody);
             processAndSendEvent(ApiId.COVERAGE_ELIGIBILITY_CHECK, header, requestBody);
             return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
         } catch (Exception e) {
@@ -28,6 +31,8 @@ public class CoverageEligibilityController extends BaseController {
     public ResponseEntity<Object> onCheckCoverageEligibility(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
         Response response = getResponse(ApiId.COVERAGE_ELIGIBILITY_ONCHECK);
         try {
+            validateHeaders(header);
+            validatePayload(requestBody);
             processAndSendEvent(ApiId.COVERAGE_ELIGIBILITY_ONCHECK, header, requestBody);
             return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
         } catch (Exception e) {
