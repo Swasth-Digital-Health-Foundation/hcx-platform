a000f17e4840c1dec7316c763746c9232f4f986c
Shiva Rakshith <rakshiths@sanketika.in>
2021-10-29 12:13:03 +0530

    added payload validations

--- hcx-apis/src/main/java/org/swasth/hcx/controllers/BaseController.java
+++ hcx-apis/src/main/java/org/swasth/hcx/controllers/BaseController.java
@@ -11,6 +11,8 @@ import org.swasth.hcx.Helpers.KafkaEventGenerator;
 import org.swasth.hcx.middleware.KafkaClient;
 import org.swasth.hcx.pojos.Response;
 import org.swasth.hcx.pojos.ResponseParams;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -38,25 +40,28 @@ public class BaseController {
         return uid.toString();
     }
 
-    public Map<String, Object> getAndValidateBody(Map<String, Object> requestBody) {
-        Map<String, Object> body = (Map<String, Object>) requestBody.get("request");
-        if(body.isEmpty())
+    public void validatePayload(Map<String, Object> requestBody) {
+        if(requestBody.isEmpty()) {
             throw new RestClientException("Request Body cannot be Empty.");
-        else
-            return body;
+        } else {
+            List<String> mandatoryPayloadProps = (List<String>) env.getProperty("payload.mandatory.properties", List.class, new ArrayList<String>());
+            List<String> missingPayloadProps = mandatoryPayloadProps.stream().filter(key -> mandatoryPayloadProps.contains(key)).collect(Collectors.toList());
+           if(!missingPayloadProps.isEmpty()) {
+               throw new RestClientException("Payload mandatory properties are missing: " + missingPayloadProps);
+           }
+        }
     }
 
     public void validateHeaders(HttpHeaders header) {
-        List<String> headerKeys = header.keySet().stream().collect(Collectors.toList());
-        List<String> mandatoryProtocalHeaders = (List<String>) env.getProperty("protocal.mandatory.headers", List.class);
-        List<String> mandatoryJoseHeaders = (List<String>) env.getProperty("jose.headers", List.class);
-        List<String> missingJoseHeaders = mandatoryJoseHeaders.stream().filter(key -> !headerKeys.contains(key)).collect(Collectors.toList());
+        List<String> mandatoryProtocolHeaders = (List<String>) env.getProperty("protocol.mandatory.headers", List.class, new ArrayList<String>());
+        List<String> mandatoryJoseHeaders = (List<String>) env.getProperty("jose.headers", List.class, new ArrayList<String>());
+        List<String> missingJoseHeaders = mandatoryJoseHeaders.stream().filter(key -> !header.containsKey(key)).collect(Collectors.toList());
         if(!missingJoseHeaders.isEmpty()) {
             throw new RestClientException("Jose mandatory headers are missing: " + missingJoseHeaders);
         }
-        List<String> missingProtocalHeaders = mandatoryProtocalHeaders.stream().filter(key -> !headerKeys.contains(key)).collect(Collectors.toList());
-        if(!missingProtocalHeaders.isEmpty()) {
-            throw new RestClientException("Protocal mandatory headers are missing: " + missingProtocalHeaders);
+        List<String> missingProtocolHeaders = mandatoryProtocolHeaders.stream().filter(key -> !header.containsKey(key)).collect(Collectors.toList());
+        if(!missingProtocolHeaders.isEmpty()) {
+            throw new RestClientException("Protocol mandatory headers are missing: " + missingProtocolHeaders);
         }
     }
 
@@ -70,16 +75,15 @@ public class BaseController {
         ResponseParams responseParams = new ResponseParams();
         responseParams.setStatus(Response.Status.UNSUCCESSFUL);
         responseParams.setErrmsg(e.getMessage());
+        response.setParams(responseParams);
         response.setResponseCode(HttpStatus.BAD_REQUEST.toString());
         return response;
     }
 
     public void processAndSendEvent(String apiId, HttpHeaders header, Map<String, Object> requestBody) throws JsonProcessingException {
-        validateHeaders(header);
-        Map<String, Object> body = getAndValidateBody(requestBody);
         String mid = getUUID();
-        String payloadEvent = kafkaEventGenerator.generatePayloadEvent(mid, body);
-        String metadataEvent = kafkaEventGenerator.generateMetadataEvent(mid, apiId, header, body);
+        String payloadEvent = kafkaEventGenerator.generatePayloadEvent(mid, requestBody);
+        String metadataEvent = kafkaEventGenerator.generateMetadataEvent(mid, apiId, header);
         kafkaClient.send(topic, key, payloadEvent);
         kafkaClient.send(topic, key, metadataEvent);
     }
