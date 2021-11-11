1f6c5fc888c94e3ec90d11d137935a6baa28ed80
Shiva Rakshith <rakshiths@sanketika.in>
2021-11-11 10:36:38 +0530

    Update PaymentsController.java

--- hcx-apis/src/main/java/org/swasth/hcx/controllers/v1/PaymentsController.java
+++ hcx-apis/src/main/java/org/swasth/hcx/controllers/v1/PaymentsController.java
@@ -1,12 +1,14 @@
 package org.swasth.hcx.controllers.v1;
 
-import org.springframework.http.HttpHeaders;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.swasth.common.StringUtils;
+import org.swasth.common.exception.ClientException;
+import org.swasth.common.exception.ResponseCode;
 import org.swasth.hcx.controllers.BaseController;
-import org.swasth.common.dto.Response;
-import org.swasth.hcx.utils.ApiId;
+import org.swasth.hcx.utils.Constants;
 
 import java.util.Map;
 
@@ -15,54 +17,58 @@ import java.util.Map;
 public class PaymentsController extends BaseController {
 
     @RequestMapping(value = "/request", method = RequestMethod.POST)
-    public ResponseEntity<Object> paymentNoticeRequest(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
-        Response response = getResponse(ApiId.PAYMENT_NOTICE_REQUEST);
+    public ResponseEntity<Object> paymentNoticeRequest(@RequestBody Map<String, Object> requestBody) throws JsonProcessingException {
+        String correlationId = StringUtils.decodeBase64String((String) requestBody.getOrDefault("protected","e30=")).getOrDefault("x-hcx-correlation_id","").toString();
         try {
-            validateHeaders(header);
-            validatePayload(requestBody);
-            processAndSendEvent(ApiId.PAYMENT_NOTICE_REQUEST, header, requestBody);
-            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+            validateRequestBody(requestBody);
+            processAndSendEvent(Constants.PAYMENT_NOTICE_REQUEST, requestBody);
+            return new ResponseEntity<>(successResponse(correlationId), HttpStatus.ACCEPTED);
+        } catch (ClientException e) {
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.CLIENT_ERROR, e), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(badRequestResponse(response, e), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.SERVER_ERROR, e), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @RequestMapping(value = "/on_request", method = RequestMethod.POST)
-    public ResponseEntity<Object> paymentNoticeOnRequest(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
-        Response response = getResponse(ApiId.PAYMENT_NOTICE_ONREQUEST);
+    public ResponseEntity<Object> paymentNoticeOnRequest(@RequestBody Map<String, Object> requestBody) throws JsonProcessingException {
+        String correlationId = StringUtils.decodeBase64String((String) requestBody.getOrDefault("protected","e30=")).getOrDefault("x-hcx-correlation_id","").toString();
         try {
-            validateHeaders(header);
-            validatePayload(requestBody);
-            processAndSendEvent(ApiId.PAYMENT_NOTICE_ONREQUEST, header, requestBody);
-            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+            validateRequestBody(requestBody);
+            processAndSendEvent(Constants.PAYMENT_NOTICE_ONREQUEST, requestBody);
+            return new ResponseEntity<>(successResponse(correlationId), HttpStatus.ACCEPTED);
+        } catch (ClientException e) {
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.CLIENT_ERROR, e), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(badRequestResponse(response, e), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.SERVER_ERROR, e), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @RequestMapping(value = "/search", method = RequestMethod.POST)
-    public ResponseEntity<Object> paymentNoticeSearch(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
-        Response response = getResponse(ApiId.PAYMENT_NOTICE_SEARCH);
+    public ResponseEntity<Object> paymentNoticeSearch(@RequestBody Map<String, Object> requestBody) throws JsonProcessingException {
+        String correlationId = StringUtils.decodeBase64String((String) requestBody.getOrDefault("protected","e30=")).getOrDefault("x-hcx-correlation_id","").toString();
         try {
-            validateHeaders(header);
-            validatePayload(requestBody);
-            processAndSendEvent(ApiId.PAYMENT_NOTICE_SEARCH, header, requestBody);
-            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+            validateRequestBody(requestBody);
+            processAndSendEvent(Constants.PAYMENT_NOTICE_SEARCH, requestBody);
+            return new ResponseEntity<>(successResponse(correlationId), HttpStatus.ACCEPTED);
+        } catch (ClientException e) {
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.CLIENT_ERROR, e), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(badRequestResponse(response, e), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.SERVER_ERROR, e), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @RequestMapping(value = "/on_search", method = RequestMethod.POST)
-    public ResponseEntity<Object> paymentNoticeOnSearch(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
-        Response response = getResponse(ApiId.PAYMENT_NOTICE_ONSEARCH);
+    public ResponseEntity<Object> paymentNoticeOnSearch(@RequestBody Map<String, Object> requestBody) throws JsonProcessingException {
+        String correlationId = StringUtils.decodeBase64String((String) requestBody.getOrDefault("protected","e30=")).getOrDefault("x-hcx-correlation_id","").toString();
         try {
-            validateHeaders(header);
-            validatePayload(requestBody);
-            processAndSendEvent(ApiId.PAYMENT_NOTICE_ONSEARCH, header, requestBody);
-            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+            validateRequestBody(requestBody);
+            processAndSendEvent(Constants.PAYMENT_NOTICE_ONSEARCH, requestBody);
+            return new ResponseEntity<>(successResponse(correlationId), HttpStatus.ACCEPTED);
+        } catch (ClientException e) {
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.CLIENT_ERROR, e), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(badRequestResponse(response, e), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.SERVER_ERROR, e), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
