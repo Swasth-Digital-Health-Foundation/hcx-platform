3f9fe9e4cd605607d5c4bdbe0afb277287f0f5f0
Shiva Rakshith <rakshiths@sanketika.in>
2021-11-11 10:36:36 +0530

    Update ClaimsController.java

--- hcx-apis/src/main/java/org/swasth/hcx/controllers/v1/ClaimsController.java
+++ hcx-apis/src/main/java/org/swasth/hcx/controllers/v1/ClaimsController.java
@@ -1,12 +1,14 @@
 package org.swasth.hcx.controllers.v1;
 
-import org.springframework.http.HttpHeaders;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.swasth.common.StringUtils;
+import org.swasth.common.exception.ClientException;
+import org.swasth.common.exception.ResponseCode;
 import org.swasth.hcx.controllers.BaseController;
-import org.swasth.common.dto.Response;
-import org.swasth.hcx.utils.ApiId;
+import org.swasth.hcx.utils.Constants;
 
 import java.util.Map;
 
@@ -14,107 +16,115 @@ import java.util.Map;
 @RequestMapping(value = "/v1")
 public class ClaimsController extends BaseController {
 
-    @RequestMapping(value = "/preauth/submit", method = RequestMethod.POST)
-    public ResponseEntity<Object> preAuthSubmit(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
-        Response response = getResponse(ApiId.PRE_AUTH_SUBMIT);
-        try {
-            validateHeaders(header);
-            validatePayload(requestBody);
-            processAndSendEvent(ApiId.PRE_AUTH_SUBMIT, header, requestBody);
-            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
-        } catch (Exception e) {
-            return new ResponseEntity<>(badRequestResponse(response, e), HttpStatus.BAD_REQUEST);
-        }
+   @RequestMapping(value = "/preauth/submit", method = RequestMethod.POST)
+    public ResponseEntity<Object> preAuthSubmit(@RequestBody Map<String, Object> requestBody) throws JsonProcessingException {
+       String correlationId = StringUtils.decodeBase64String((String) requestBody.getOrDefault("protected","e30=")).getOrDefault("x-hcx-correlation_id","").toString();
+       try {
+           validateRequestBody(requestBody);
+           processAndSendEvent(Constants.PRE_AUTH_SUBMIT, requestBody);
+           return new ResponseEntity<>(successResponse(correlationId), HttpStatus.ACCEPTED);
+       } catch (ClientException e) {
+           return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.CLIENT_ERROR, e), HttpStatus.BAD_REQUEST);
+       } catch (Exception e) {
+           return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.SERVER_ERROR, e), HttpStatus.INTERNAL_SERVER_ERROR);
+       }
     }
 
     @RequestMapping(value = "/preauth/on_submit", method = RequestMethod.POST)
-    public ResponseEntity<Object> preAuthOnSubmit(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
-        Response response = getResponse(ApiId.PRE_AUTH_ONSUBMIT);
+    public ResponseEntity<Object> preAuthOnSubmit(@RequestBody Map<String, Object> requestBody) throws JsonProcessingException {
+        String correlationId = StringUtils.decodeBase64String((String) requestBody.getOrDefault("protected","e30=")).getOrDefault("x-hcx-correlation_id","").toString();
         try {
-            validateHeaders(header);
-            validatePayload(requestBody);
-            processAndSendEvent(ApiId.PRE_AUTH_ONSUBMIT, header, requestBody);
-            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+            validateRequestBody(requestBody);
+            processAndSendEvent(Constants.PRE_AUTH_ONSUBMIT, requestBody);
+            return new ResponseEntity<>(successResponse(correlationId), HttpStatus.ACCEPTED);
+        } catch (ClientException e) {
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.CLIENT_ERROR, e), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(badRequestResponse(response, e), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.SERVER_ERROR, e), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @RequestMapping(value = "/preauth/search", method = RequestMethod.POST)
-    public ResponseEntity<Object> preAuthSearch(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
-        Response response = getResponse(ApiId.PRE_AUTH_SEARCH);
+    public ResponseEntity<Object> preAuthSearch(@RequestBody Map<String, Object> requestBody) throws JsonProcessingException {
+        String correlationId = StringUtils.decodeBase64String((String) requestBody.getOrDefault("protected","e30=")).getOrDefault("x-hcx-correlation_id","").toString();
         try {
-            validateHeaders(header);
-            validatePayload(requestBody);
-            processAndSendEvent(ApiId.PRE_AUTH_SEARCH, header, requestBody);
-            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+            validateRequestBody(requestBody);
+            processAndSendEvent(Constants.PRE_AUTH_SEARCH, requestBody);
+            return new ResponseEntity<>(successResponse(correlationId), HttpStatus.ACCEPTED);
+        } catch (ClientException e) {
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.CLIENT_ERROR, e), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(badRequestResponse(response, e), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.SERVER_ERROR, e), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @RequestMapping(value = "/preauth/on_search", method = RequestMethod.POST)
-    public ResponseEntity<Object> preAuthOnSearch(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
-        Response response = getResponse(ApiId.PRE_AUTH_ONSEARCH);
+    public ResponseEntity<Object> preAuthOnSearch(@RequestBody Map<String, Object> requestBody) throws JsonProcessingException {
+        String correlationId = StringUtils.decodeBase64String((String) requestBody.getOrDefault("protected","e30=")).getOrDefault("x-hcx-correlation_id","").toString();
         try {
-            validateHeaders(header);
-            validatePayload(requestBody);
-            processAndSendEvent(ApiId.PRE_AUTH_ONSEARCH, header, requestBody);
-            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+            validateRequestBody(requestBody);
+            processAndSendEvent(Constants.PRE_AUTH_ONSEARCH, requestBody);
+            return new ResponseEntity<>(successResponse(correlationId), HttpStatus.ACCEPTED);
+        } catch (ClientException e) {
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.CLIENT_ERROR, e), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(badRequestResponse(response, e), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.SERVER_ERROR, e), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @RequestMapping(value = "/claim/submit", method = RequestMethod.POST)
-    public ResponseEntity<Object> claimSubmit(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
-        Response response = getResponse(ApiId.CLAIM_SUBMIT);
+    public ResponseEntity<Object> claimSubmit(@RequestBody Map<String, Object> requestBody) throws JsonProcessingException {
+        String correlationId = StringUtils.decodeBase64String((String) requestBody.getOrDefault("protected","e30=")).getOrDefault("x-hcx-correlation_id","").toString();
         try {
-            validateHeaders(header);
-            validatePayload(requestBody);
-            processAndSendEvent(ApiId.CLAIM_SUBMIT, header, requestBody);
-            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+            validateRequestBody(requestBody);
+            processAndSendEvent(Constants.CLAIM_SUBMIT, requestBody);
+            return new ResponseEntity<>(successResponse(correlationId), HttpStatus.ACCEPTED);
+        } catch (ClientException e) {
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.CLIENT_ERROR, e), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(badRequestResponse(response, e), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.SERVER_ERROR, e), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @RequestMapping(value = "/claim/on_submit", method = RequestMethod.POST)
-    public ResponseEntity<Object> claimOnSubmit(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
-        Response response = getResponse(ApiId.CLAIM_ONSUBMIT);
+    public ResponseEntity<Object> claimOnSubmit(@RequestBody Map<String, Object> requestBody) throws JsonProcessingException {
+        String correlationId = StringUtils.decodeBase64String((String) requestBody.getOrDefault("protected","e30=")).getOrDefault("x-hcx-correlation_id","").toString();
         try {
-            validateHeaders(header);
-            validatePayload(requestBody);
-            processAndSendEvent(ApiId.CLAIM_ONSUBMIT, header, requestBody);
-            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+            validateRequestBody(requestBody);
+            processAndSendEvent(Constants.CLAIM_ONSUBMIT, requestBody);
+            return new ResponseEntity<>(successResponse(correlationId), HttpStatus.ACCEPTED);
+        } catch (ClientException e) {
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.CLIENT_ERROR, e), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(badRequestResponse(response, e), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.SERVER_ERROR, e), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @RequestMapping(value = "/claim/search", method = RequestMethod.POST)
-    public ResponseEntity<Object> claimSearch(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
-        Response response = getResponse(ApiId.CLAIM_SEARCH);
+    public ResponseEntity<Object> claimSearch(@RequestBody Map<String, Object> requestBody) throws JsonProcessingException {
+        String correlationId = StringUtils.decodeBase64String((String) requestBody.getOrDefault("protected","e30=")).getOrDefault("x-hcx-correlation_id","").toString();
         try {
-            validateHeaders(header);
-            validatePayload(requestBody);
-            processAndSendEvent(ApiId.CLAIM_SEARCH, header, requestBody);
-            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+            validateRequestBody(requestBody);
+            processAndSendEvent(Constants.CLAIM_SEARCH, requestBody);
+            return new ResponseEntity<>(successResponse(correlationId), HttpStatus.ACCEPTED);
+        } catch (ClientException e) {
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.CLIENT_ERROR, e), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(badRequestResponse(response, e), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.SERVER_ERROR, e), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @RequestMapping(value = "/claim/on_search", method = RequestMethod.POST)
-    public ResponseEntity<Object> claimOnSearch(@RequestHeader HttpHeaders header, @RequestBody Map<String, Object> requestBody) {
-        Response response = getResponse(ApiId.CLAIM_ONSEARCH);
+    public ResponseEntity<Object> claimOnSearch(@RequestBody Map<String, Object> requestBody) throws JsonProcessingException {
+        String correlationId = StringUtils.decodeBase64String((String) requestBody.getOrDefault("protected","e30=")).getOrDefault("x-hcx-correlation_id","").toString();
         try {
-            validateHeaders(header);
-            validatePayload(requestBody);
-            processAndSendEvent(ApiId.CLAIM_ONSEARCH, header, requestBody);
-            return new ResponseEntity<>(response, HttpStatus.ACCEPTED);
+            validateRequestBody(requestBody);
+            processAndSendEvent(Constants.CLAIM_ONSEARCH, requestBody);
+            return new ResponseEntity<>(successResponse(correlationId), HttpStatus.ACCEPTED);
+        } catch (ClientException e) {
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.CLIENT_ERROR, e), HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
-            return new ResponseEntity<>(badRequestResponse(response, e), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorResponse(correlationId, ResponseCode.SERVER_ERROR, e), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
