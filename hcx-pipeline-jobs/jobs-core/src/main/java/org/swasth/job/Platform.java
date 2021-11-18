86221689d47592dfbcf645499cbd324996a3c813
Shiva Rakshith <rakshiths@sanketika.in>
2021-11-18 18:56:32 +0530

    Update Platform.java

--- hcx-pipeline-jobs/jobs-core/src/main/java/org/swasth/job/Platform.java
+++ hcx-pipeline-jobs/jobs-core/src/main/java/org/swasth/job/Platform.java
@@ -11,8 +11,6 @@ public class Platform {
     public static Config config = envConf.withFallback(defaultConf);
 
     private static int requestTimeout = 30;
-    private static Map<String, List<String>> graphIds = new HashMap<>();
-
 
     public static void loadProperties(Config conf) {
         config = config.withFallback(conf);
@@ -20,26 +18,6 @@ public class Platform {
 
     public static int getTimeout() {
         return requestTimeout;
-    }
-
-    public static List<String> getGraphIds(String... services) {
-        List<String> ids = new ArrayList<>();
-        for (String service: services) {
-            ids.addAll(getGraphIds(service));
-        }
-        return ids;
-    }
-
-    private static List<String> getGraphIds(String service) {
-        service = service.toLowerCase();
-        if (!graphIds.containsKey(service)) {
-            String key = service + ".graph_ids";
-            if (config.hasPath(key)) {
-                graphIds.put(service, config.getStringList(key));
-            } else
-                return Arrays.asList();
-        }
-        return graphIds.get(service);
     }
 
     public static String getString(String key) {
