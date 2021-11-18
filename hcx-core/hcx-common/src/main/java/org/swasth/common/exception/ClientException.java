d5d3a23720461a94d12ca354eeea2843996978f7
Shiva Rakshith <rakshiths@sanketika.in>
2021-11-18 11:25:33 +0530

    Update ClientException.java

--- hcx-core/hcx-common/src/main/java/org/swasth/common/exception/ClientException.java
+++ hcx-core/hcx-common/src/main/java/org/swasth/common/exception/ClientException.java
@@ -2,7 +2,14 @@ package org.swasth.common.exception;
 
 public class ClientException extends Exception{
 
+    private String errCode;
+
     public ClientException(String message) {
         super(message);
+    }
+
+    public ClientException(String errCode, String message) {
+        super(message);
+        this.errCode = errCode;
     }
 }
