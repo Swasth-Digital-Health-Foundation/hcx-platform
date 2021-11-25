e38cbe90e67a797c3ddb167ebead7757a4346b2a
Shiva Rakshith <rakshiths@sanketika.in>
2021-11-25 18:36:11 +0530

    Update DenormaliserFunction

--- hcx-pipeline-jobs/denormaliser/src/main/java/org/swasth/job/functions/DenormaliserFunction.java
+++ hcx-pipeline-jobs/denormaliser/src/main/java/org/swasth/job/functions/DenormaliserFunction.java
@@ -2,23 +2,29 @@ package org.swasth.job.functions;
 
 import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.util.Collector;
+import org.swasth.job.helpers.RegistryFetcher;
 import org.swasth.job.task.DenormaliserConfig;
 import org.swasth.job.utils.JSONUtil;
-
 import java.util.Map;
 
-public class DenormaliserFunction extends ProcessFunction<String,String> {
-
-    Denormaliser denormaliser = new Denormaliser();
+public class DenormaliserFunction extends ProcessFunction<String,String> implements RegistryFetcher {
 
     @Override
     public void processElement(String event, ProcessFunction<String, String>.Context context, Collector<String> collector) throws Exception {
-        Map<String, Object> result = denormaliser.denormalise(event);
-        if(result.get("status").equals("request.invalid")) {
-            context.output(DenormaliserConfig.invalidOutTag, JSONUtil.serialize(result));
+        Map<String, Object> eventMap = JSONUtil.deserializeToMap(event);
+        String senderCode = (String) ((Map<String, Object>) eventMap.get("sender")).getOrDefault("participant_code", "");
+        String receiverCode = (String) ((Map<String, Object>) eventMap.get("receiver")).getOrDefault("participant_code", "");
+        Map<String, Object> senderDetails = getParticipantDetails(senderCode);
+        Map<String, Object> receiverDetails = getParticipantDetails(receiverCode);
+        ((Map<String, Object>) eventMap.get("sender")).putAll(senderDetails);
+        if(receiverDetails.containsKey("error")) {
+            eventMap.put("status","request.invalid");
+            eventMap.put("log_details",receiverDetails.get("error"));
+            context.output(DenormaliserConfig.invalidOutTag, JSONUtil.serialize(eventMap));
         } else {
-            result.put("status", "request.denorm");
-            collector.collect(JSONUtil.serialize(result));
+            ((Map<String, Object>) eventMap.get("receiver")).putAll(receiverDetails);
+            eventMap.put("status", "request.denorm");
+            collector.collect(JSONUtil.serialize(eventMap));
         }
     }
 }
