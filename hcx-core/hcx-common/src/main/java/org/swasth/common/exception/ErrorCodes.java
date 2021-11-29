64e88135cf6c7ad18b933b658f27b4d3c78e67c5
Shiva Rakshith <rakshiths@sanketika.in>
2021-11-29 12:56:05 +0530

    Revert "Merge branch 'main' into pipeline-jobs"
    This reverts commit 4abecb7b368e1ce46dc7ef60ccbf243ffe1a7eda, reversing
    changes made to efcd6fa33bc0dc3995745d6dc262729d874eb1ac.

--- hcx-core/hcx-common/src/main/java/org/swasth/common/exception/ErrorCodes.java
+++
@@ -1,5 +0,0 @@
-package org.swasth.common.exception;
-
-public enum ErrorCodes {
-    CLIENT_ERR_EMPTY_REQ_BODY, CLIENT_ERR_INVALID_PAYLOAD, CLIENT_ERR_INVALID_HEADER, CLIENT_ERR_INVALID_TOPIC, SERVER_ERROR
-}
