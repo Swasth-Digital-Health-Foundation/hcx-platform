1ca809ba32d9e14469135299c100d34ef38456ef
Shiva Rakshith <rakshiths@sanketika.in>
2021-12-01 12:04:04 +0530

    Update StringUtils.java

--- hcx-pipeline-jobs/jobs-core/src/main/java/org/swasth/job/utils/StringUtils.java
+++ hcx-pipeline-jobs/jobs-core/src/main/java/org/swasth/job/utils/StringUtils.java
@@ -3,13 +3,9 @@ package org.swasth.job.utils;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
-
 public class StringUtils {
 
-    private static ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     public static String serialize(Object obj) throws JsonProcessingException {
         return mapper.writeValueAsString(obj);
