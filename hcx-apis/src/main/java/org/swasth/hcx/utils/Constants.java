382c377063c0a912c5ee724fc4124898e10d08a4
Shiva Rakshith <rakshiths@sanketika.in>
2021-12-06 17:42:47 +0530

    Update Constants.java

--- hcx-apis/src/main/java/org/swasth/hcx/utils/Constants.java
+++ hcx-apis/src/main/java/org/swasth/hcx/utils/Constants.java
@@ -3,24 +3,24 @@ package org.swasth.hcx.utils;
 public  class Constants {
 
     // Coverage Eligibility APIs
-    public static final String COVERAGE_ELIGIBILITY_CHECK = "coverageeligibility/check";
-    public static final String COVERAGE_ELIGIBILITY_ONCHECK = "coverageeligibility/oncheck";
+    public static final String COVERAGE_ELIGIBILITY_CHECK = "/v1/coverageeligibility/check";
+    public static final String COVERAGE_ELIGIBILITY_ONCHECK = "/v1/coverageeligibility/oncheck";
 
     // Claims APIs
-    public static final String PRE_AUTH_SUBMIT = "preauth/submit";
-    public static final String PRE_AUTH_ONSUBMIT = "preauth/onsubmit";
-    public static final String PRE_AUTH_SEARCH = "preauth/search";
-    public static final String PRE_AUTH_ONSEARCH = "preauth/onsearch";
-    public static final String CLAIM_SUBMIT = "claim/submit";
-    public static final String CLAIM_ONSUBMIT = "claim/onsubmit";
-    public static final String CLAIM_SEARCH = "claim/search";
-    public static final String CLAIM_ONSEARCH = "claim/onsearch";
+    public static final String PRE_AUTH_SUBMIT = "/v1/preauth/submit";
+    public static final String PRE_AUTH_ONSUBMIT = "/v1/preauth/onsubmit";
+    public static final String PRE_AUTH_SEARCH = "/v1/preauth/search";
+    public static final String PRE_AUTH_ONSEARCH = "/v1/preauth/onsearch";
+    public static final String CLAIM_SUBMIT = "/v1/claim/submit";
+    public static final String CLAIM_ONSUBMIT = "/v1/claim/onsubmit";
+    public static final String CLAIM_SEARCH = "/v1/claim/search";
+    public static final String CLAIM_ONSEARCH = "/v1/claim/onsearch";
 
     //Payment Notice APIs
-    public static final String PAYMENT_NOTICE_REQUEST = "paymentnotice/request";
-    public static final String PAYMENT_NOTICE_ONREQUEST = "paymentnotice/onrequest";
-    public static final String PAYMENT_NOTICE_SEARCH = "paymentnotice/search";
-    public static final String PAYMENT_NOTICE_ONSEARCH = "paymentnotice/onsearch";
+    public static final String PAYMENT_NOTICE_REQUEST = "/v1/paymentnotice/request";
+    public static final String PAYMENT_NOTICE_ONREQUEST = "/v1/paymentnotice/onrequest";
+    public static final String PAYMENT_NOTICE_SEARCH = "/v1/paymentnotice/search";
+    public static final String PAYMENT_NOTICE_ONSEARCH = "/v1/paymentnotice/onsearch";
 
     public static final String HEALTHY = "healthy";
     public static final String NAME = "name";
