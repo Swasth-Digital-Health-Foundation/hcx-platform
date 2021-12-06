174080ec6ab0df65f41dcaac29c83d5d80dd3318
Mahesh Kumar Gangula <maheshkumargangula@users.noreply.github.com>
2021-12-07 00:54:41 +0530

    feat: logging changes to understand the status.

--- hcx-pipeline-jobs/coverage-eligibility-check-job/src/main/scala/org/swasth/dp/coverageeligiblitycheck/functions/CoverageEligibilityProcessFunction.scala
+++ hcx-pipeline-jobs/coverage-eligibility-check-job/src/main/scala/org/swasth/dp/coverageeligiblitycheck/functions/CoverageEligibilityProcessFunction.scala
@@ -3,6 +3,7 @@ package org.swasth.dp.coverageeligiblitycheck.functions
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.ProcessFunction
+import org.slf4j.LoggerFactory
 import org.swasth.dp.core.function.{BaseDispatcherFunction, ValidationResult}
 import org.swasth.dp.core.job.Metrics
 import org.swasth.dp.core.util.{JSONUtil, PostgresConnect, PostgresConnectionConfig}
@@ -12,6 +13,8 @@ import java.util
 
 class CoverageEligibilityProcessFunction(config: CoverageEligibilityCheckConfig, @transient var postgresConnect: PostgresConnect = null)(implicit val stringTypeInfo: TypeInformation[String])
   extends BaseDispatcherFunction(config) {
+
+  private[this] val logger = LoggerFactory.getLogger(classOf[CoverageEligibilityProcessFunction])
 
   override def open(parameters: Configuration): Unit = {
     super.open(parameters)
@@ -34,11 +37,15 @@ class CoverageEligibilityProcessFunction(config: CoverageEligibilityCheckConfig,
 
   override def validate(event: util.Map[String, AnyRef]): ValidationResult = {
     // TODO: Add domain specific validations
+    Console.println("Triggered validation for event: " + event)
+    logger.info("Triggered validation for event: " + event)
     ValidationResult(true, None)
   }
 
   override def getPayload(event: util.Map[String, AnyRef]): util.Map[String, AnyRef] = {
-    val payloadRefId = getPayloadRefId(event);
+    val payloadRefId = getPayloadRefId(event)
+    Console.println("Fetching payload from postgres for mid: " + payloadRefId)
+    logger.info("Fetching payload from postgres for mid: " + payloadRefId)
     val postgresQuery = String.format("SELECT data FROM %s WHERE mid = '%s'", config.payloadTable, payloadRefId);
     val preparedStatement = postgresConnect.getConnection.prepareStatement(postgresQuery)
     try {
@@ -47,7 +54,7 @@ class CoverageEligibilityProcessFunction(config: CoverageEligibilityCheckConfig,
         val payload = resultSet.getString(1)
         JSONUtil.deserialize[util.Map[String, AnyRef]](payload)
       } else {
-        throw new Exception("Payload not found for the given reference id")
+        throw new Exception("Payload not found for the given reference id: " + payloadRefId)
       }
     } catch {
       case ex: Exception => throw ex
