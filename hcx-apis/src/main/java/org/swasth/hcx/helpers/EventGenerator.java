f94a563e4830de44fb8047bc6dcc857427e4773d
Shiva Rakshith <rakshiths@sanketika.in>
2021-12-17 19:38:16 +0530

    fix: remove domain headers

--- hcx-apis/src/main/java/org/swasth/hcx/helpers/EventGenerator.java
+++ hcx-apis/src/main/java/org/swasth/hcx/helpers/EventGenerator.java
@@ -27,12 +27,10 @@ public class EventGenerator {
         Map<String,Object> event = new HashMap<>();
         List<String> protocolHeaders = env.getProperty(Constants.PROTOCOL_HEADERS_MANDATORY, List.class);
         protocolHeaders.addAll(env.getProperty(Constants.PROTOCOL_HEADERS_OPTIONAL, List.class));
-        List<String> domainHeaders = env.getProperty(Constants.DOMAIN_HEADERS, List.class);
         List<String> joseHeaders = env.getProperty(Constants.JOSE_HEADERS, List.class);
         HashMap<String,Object> protectedHeaders = StringUtils.decodeBase64String((String) requestBody.get(Constants.PROTECTED), HashMap.class);
         Map<String,Object> filterJoseHeaders = new HashMap<>();
         Map<String,Object> filterProtocolHeaders = new HashMap<>();
-        Map<String,Object> filterDomainHeaders = new HashMap<>();
         joseHeaders.forEach(key -> {
             if (protectedHeaders.containsKey(key))
                 filterJoseHeaders.put(key, protectedHeaders.get(key));
@@ -41,17 +39,12 @@ public class EventGenerator {
             if (protectedHeaders.containsKey(key))
                 filterProtocolHeaders.put(key, protectedHeaders.get(key));
         });
-        domainHeaders.forEach(key -> {
-            if (protectedHeaders.containsKey(key))
-                filterDomainHeaders.put(key, protectedHeaders.get(key));
-        });
         event.put(Constants.MID, mid);
         event.put(Constants.ETS, System.currentTimeMillis());
         event.put(Constants.ACTION, apiAction);
         event.put(Constants.HEADERS, new HashMap<>(){{
             put(Constants.JOSE, filterJoseHeaders);
             put(Constants.PROTOCOL, filterProtocolHeaders);
-            put(Constants.DOMAIN, filterDomainHeaders);
         }});
         event.put(Constants.LOG_DETAILS, new HashMap<>(){{
             put(Constants.CODE, "");
