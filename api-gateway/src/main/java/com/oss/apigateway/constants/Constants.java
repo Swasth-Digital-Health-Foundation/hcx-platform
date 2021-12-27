f3ade0381c2e9dcff4a7bf5b4916d0091980568c
Shiva Rakshith <rakshiths@sanketika.in>
2021-12-27 15:27:11 +0530

    feat: add caller id validation

--- api-gateway/src/main/java/com/oss/apigateway/constants/Constants.java
+++ api-gateway/src/main/java/com/oss/apigateway/constants/Constants.java
@@ -4,7 +4,10 @@ public class Constants {
     public static final String AUTH_REQUIRED = "AUTH_REQUIRED";
     public static final String X_JWT_SUB_HEADER = "X-jwt-sub";
     public static final String CORRELATION_ID = "x-hcx-correlation_id";
+    public static final String AUTHORIZATION = "Authorization";
     public static final String PAYLOAD = "payload";
     public static final String SENDER_CODE = "x-hcx-sender_code";
     public static final String RECIPIENT_CODE = "x-hcx-recipient_code";
+    public static final String OS_OWNER = "osOwner";
+
 }
--- api-gateway/src/main/java/com/oss/apigateway/filters/RegistryValidationFilter.java
+++ api-gateway/src/main/java/com/oss/apigateway/filters/RegistryValidationFilter.java
@@ -1,7 +1,10 @@
 package com.oss.apigateway.filters;
 
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.jayway.jsonpath.JsonPath;
 import com.oss.apigateway.cache.RedisCache;
 import com.oss.apigateway.constants.Constants;
 import com.oss.apigateway.exception.ClientException;
@@ -11,15 +14,20 @@ import com.oss.apigateway.models.ResponseError;
 import com.oss.apigateway.utils.HttpUtils;
 import com.oss.apigateway.utils.JSONUtils;
 import kong.unirest.HttpResponse;
+import kong.unirest.json.JSONString;
+import net.minidev.json.JSONArray;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -27,6 +35,7 @@ import reactor.core.publisher.Mono;
 
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -36,6 +45,7 @@ import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.C
 public class RegistryValidationFilter extends AbstractGatewayFilterFactory<RegistryValidationFilter.Config> {
 
     private static final Logger logger = LoggerFactory.getLogger(RegistryValidationFilter.class);
+    private final JWTVerifier jwtVerifier;
 
     @Autowired
     RedisCache redisCache;
@@ -43,10 +53,9 @@ public class RegistryValidationFilter extends AbstractGatewayFilterFactory<Regis
     @Value("${registry.basePath}")
     private String registryUrl;
 
-    private static final String WWW_VALIDATE_HEADER = "WWW-Validate";
-
-    public RegistryValidationFilter() {
+    public RegistryValidationFilter(@Qualifier("jwk") JWTVerifier jwtVerifier) {
         super(Config.class);
+        this.jwtVerifier = jwtVerifier;
     }
 
     @Override
@@ -60,13 +69,22 @@ public class RegistryValidationFilter extends AbstractGatewayFilterFactory<Regis
                 try {
                     protectedMap = JSONUtils.decodeBase64String(requestBody[0], HashMap.class);
                 } catch (JsonParseException e) {
-                    throw new ClientException(ErrorCodes.CLIENT_ERR_INVALID_REQ_PROTECTED, "Error while parsing protected headers");
+                    throw new ClientException(ErrorCodes.CLIENT_ERR_WRONG_ENCODED_PROTECTED, "Error while parsing protected headers");
+                }
+                if (!protectedMap.containsKey(Constants.CORRELATION_ID) || !(protectedMap.get(Constants.CORRELATION_ID) instanceof String) || ((String) protectedMap.get(Constants.CORRELATION_ID)).isEmpty()) {
+                    throw new ClientException(ErrorCodes.CLIENT_ERR_INVALID_CORREL_ID, "Correlation id cannot be null, empty and other than 'String'");
                 }
                 corrId = (String) protectedMap.get(Constants.CORRELATION_ID);
-                String senderCode = (String) protectedMap.get(Constants.SENDER_CODE);
-                String recipientCode = (String) protectedMap.get(Constants.RECIPIENT_CODE);
-                Map<String,Object> senderDetails = getDetails(senderCode);
-                Map<String,Object> recipientDetails = getDetails(recipientCode);
+                Object senderCode = protectedMap.get(Constants.SENDER_CODE);
+                Object recipientCode = protectedMap.get(Constants.RECIPIENT_CODE);
+                if (!(senderCode instanceof String) || ((String) senderCode).isEmpty()) {
+                    throw new ClientException(ErrorCodes.CLIENT_ERR_INVALID_SENDER, "Sender code cannot be null, empty and other than 'String'");
+                }
+                if (!(recipientCode instanceof String) || ((String) recipientCode).isEmpty()) {
+                    throw new ClientException(ErrorCodes.CLIENT_ERR_INVALID_RECIPIENT, "Recipient code cannot be null, empty and other than 'String'");
+                }
+                Map<String,Object> senderDetails = getDetails(senderCode.toString());
+                Map<String,Object> recipientDetails = getDetails(recipientCode.toString());
                 if (senderCode.equals(recipientCode)) {
                     throw new ClientException("sender and recipient code cannot be the same");
                 }
@@ -76,6 +94,7 @@ public class RegistryValidationFilter extends AbstractGatewayFilterFactory<Regis
                 if (recipientDetails.isEmpty()) {
                     throw new ClientException(ErrorCodes.CLIENT_ERR_INVALID_RECIPIENT, "Recipient is not exist in registry");
                 }
+                validateCallerId(exchange, senderDetails);
             } catch (ClientException e) {
                 logger.error(e.toString());
                 return this.onError(exchange, HttpStatus.BAD_REQUEST, corrId, e.getErrCode(), e);
@@ -87,6 +106,16 @@ public class RegistryValidationFilter extends AbstractGatewayFilterFactory<Regis
         };
     }
 
+    private void validateCallerId(ServerWebExchange exchange, Map<String,Object> senderDetails) throws ClientException {
+        ServerHttpRequest request = exchange.getRequest();
+        String token = request.getHeaders().get(Constants.AUTHORIZATION).get(0).trim().split("\\s")[1].trim();
+        DecodedJWT decodedJWT = this.jwtVerifier.verify(token);
+        String subject = decodedJWT.getSubject();
+        if(!StringUtils.equals(((ArrayList) senderDetails.get(Constants.OS_OWNER)).get(0).toString(),subject)){
+            throw new ClientException(ErrorCodes.CLIENT_ERR_INVALID_CALLER_ID, "Caller id is mismatched");
+        }
+    }
+
     private String[] formatRequestBody(Map<String, Object> requestBody) throws Exception {
         try {
             String str = (String) requestBody.get(Constants.PAYLOAD);
@@ -96,17 +125,17 @@ public class RegistryValidationFilter extends AbstractGatewayFilterFactory<Regis
         }
     }
 
-    private Map<String,Object> getDetails(String entityId) throws Exception {
-        String details = redisCache.get(entityId);
+    private Map<String,Object> getDetails(String code) throws Exception {
+        String details = redisCache.get(code);
         if(details == null || details.isEmpty()) {
             String url = registryUrl + "/api/v1/Organisation/search";
-            String requestBody = "{\"entityType\":[\"Organisation\"],\"filters\":{\"osid\":{\"eq\":\"" + entityId + "\"}}}";
+            String requestBody = "{\"entityType\":[\"Organisation\"],\"filters\":{\"osid\":{\"eq\":\"" + code + "\"}}}";
             HttpResponse response = HttpUtils.post(url, requestBody);
             if (response != null && response.getStatus() == 200) {
-                ArrayList result = JSONUtils.deserialize(response.getBody().toString(), ArrayList.class);
+                ArrayList result = JSONUtils.deserialize((String) response.getBody(), ArrayList.class);
                 if (!result.isEmpty()){
-                    details = (String) result.get(0);
-                    redisCache.set(entityId,result.get(0).toString());
+                    details = JSONUtils.serialize(result.get(0));
+                    redisCache.set(code, details);
                 }
             }
             else {
@@ -123,6 +152,7 @@ public class RegistryValidationFilter extends AbstractGatewayFilterFactory<Regis
         try {
             byte[] obj = JSONUtils.convertToByte(resp);
             response.setStatusCode(status);
+            response.getHeaders().add("Content-Type", "application/json");
             return response.writeWith(Mono.just(obj).map(r -> dataBufferFactory.wrap(r)));
         } catch (JsonProcessingException ex) {
             logger.error(ex.toString());
