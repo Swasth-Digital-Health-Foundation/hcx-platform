90a0e1fad6269c113c99c887b1341d1da851652c
Shiva Rakshith <rakshiths@sanketika.in>
2021-12-27 15:56:10 +0530

    feat: update response annotation

--- api-gateway/src/main/java/com/oss/apigateway/models/Response.java
+++ api-gateway/src/main/java/com/oss/apigateway/models/Response.java
@@ -1,9 +1,11 @@
 package com.oss.apigateway.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response {
 
     private long timestamp = System.currentTimeMillis();
@@ -11,39 +13,12 @@ public class Response {
     private String correlationId;
     private ResponseError error;
 
-
-    public Response() {}
-
     public Response(String correlationId) {
         this.correlationId = correlationId;
     }
 
     public Response(String correlationId, ResponseError error) {
         this.correlationId = correlationId;
-        this.error = error;
-    }
-
-    public Long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Long timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public String getCorrelationId() {
-        return correlationId;
-    }
-
-    public void setCorrelationId(String correlationId) {
-        this.correlationId = correlationId;
-    }
-
-    public ResponseError getError() {
-        return error;
-    }
-
-    public void setError(ResponseError error) {
         this.error = error;
     }
 
--- api-gateway/src/main/java/com/oss/apigateway/models/ResponseError.java
+++ api-gateway/src/main/java/com/oss/apigateway/models/ResponseError.java
@@ -5,40 +5,9 @@ import lombok.Data;
 
 @Data
 public class ResponseError {
-    private ErrorCodes code;
-    private String message;
-    private Throwable trace;
 
-    public ResponseError() {}
-
-    public ResponseError(ErrorCodes code, String message, Throwable trace) {
-        this.code = code;
-        this.message = message;
-        this.trace = trace;
-    }
-
-    public ErrorCodes getCode() {
-        return code;
-    }
-
-    public void setCode(ErrorCodes code) {
-        this.code = code;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Throwable getTrace() {
-        return trace;
-    }
-
-    public void setTrace(Throwable trace) {
-        this.trace = trace;
-    }
+    private final ErrorCodes code;
+    private final String message;
+    private final Throwable trace;
 
 }
