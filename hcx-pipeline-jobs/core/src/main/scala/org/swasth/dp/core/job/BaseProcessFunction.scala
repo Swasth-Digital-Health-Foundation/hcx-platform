f130c396d1882199d22bf64edadcedec654b2aab
Naga Prasad Moka <92975412+prasadmoka@users.noreply.github.com>
2022-01-12 19:09:52 +0530

    fix:framework related changes

--- hcx-pipeline-jobs/core/src/main/scala/org/swasth/dp/core/job/BaseProcessFunction.scala
+++ hcx-pipeline-jobs/core/src/main/scala/org/swasth/dp/core/job/BaseProcessFunction.scala
@@ -1,12 +1,17 @@
 package org.swasth.dp.core.job
 
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.atomic.AtomicLong
-
+import org.apache.commons.lang3.StringUtils
 import org.apache.flink.api.scala.metrics.ScalaGauge
 import org.apache.flink.configuration.Configuration
 import org.apache.flink.streaming.api.functions.ProcessFunction
 import org.apache.flink.util.Collector
+import org.swasth.dp.core.cache.{DataCache, RedisConnect}
+import org.swasth.dp.core.util.{DispatcherUtil, JSONUtil}
+
+import java.util
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.atomic.AtomicLong
+import scala.collection.JavaConverters._
 
 case class Metrics(metrics: ConcurrentHashMap[String, AtomicLong]) {
   def incCounter(metric: String): Unit = {
@@ -29,12 +34,17 @@ trait JobMetrics {
 abstract class BaseProcessFunction[T, R](config: BaseJobConfig) extends ProcessFunction[T, R] with BaseDeduplication with JobMetrics {
 
   private val metrics: Metrics = registerMetrics(metricsList())
+  private var registryDataCache: DataCache = _
 
   override def open(parameters: Configuration): Unit = {
     metricsList().map { metric =>
       getRuntimeContext.getMetricGroup.addGroup(config.jobName)
         .gauge[Long, ScalaGauge[Long]](metric, ScalaGauge[Long]( () => metrics.getAndReset(metric) ))
     }
+    registryDataCache =
+      new DataCache(config, new RedisConnect(config.redisHost, config.redisPort, config),
+        config.redisAssetStore, config.senderReceiverFields)
+    registryDataCache.init()
   }
 
   def processElement(event: T, context: ProcessFunction[T, R]#Context, metrics: Metrics): Unit
@@ -42,5 +52,81 @@ abstract class BaseProcessFunction[T, R](config: BaseJobConfig) extends ProcessF
 
   override def processElement(event: T, context: ProcessFunction[T, R]#Context, out: Collector[R]): Unit = {
     processElement(event, context, metrics)
+  }
+
+  def getCode(event: util.Map[String, AnyRef], key: String): String = {
+    val protocolMap = event.get("headers").asInstanceOf[util.Map[String, AnyRef]].get("protocol").asInstanceOf[util.Map[String, AnyRef]]
+    protocolMap.get(key).asInstanceOf[String]
+  }
+
+  def getReplacedAction(actionStr: String): String = {
+    var replacedAction = actionStr
+    val lastVal = actionStr.split("/").last
+    if (!lastVal.startsWith("on_"))
+      replacedAction = actionStr.replace(lastVal,"on_"+lastVal)
+    replacedAction
+  }
+
+  def createSenderContext(sender: util.Map[String, AnyRef],actionStr: String): util.Map[String, AnyRef] = {
+    //Sender Details
+    var endpointUrl = sender.getOrDefault("endpoint_url", "").asInstanceOf[String]
+    if (!StringUtils.isEmpty(endpointUrl)) {
+      //If endPointUrl comes with /, remove it as action starts with /
+      if (endpointUrl.endsWith("/"))
+        endpointUrl = endpointUrl.substring(0, endpointUrl.length - 1)
+
+      // fetch on_ action for the sender
+      val replacedAction: String = getReplacedAction(actionStr)
+      val appendedSenderUrl = endpointUrl.concat(replacedAction)
+      sender.put("endpoint_url", appendedSenderUrl)
+      sender
+    } else new util.HashMap[String, AnyRef]()
+
+  }
+
+  def createRecipientContext(receiver: util.Map[String, AnyRef],actionStr: String): util.Map[String, AnyRef] = {
+    //Receiver Details
+    var endpointUrl = receiver.get("endpoint_url").asInstanceOf[String]
+    if (!StringUtils.isEmpty(endpointUrl)) {
+      //If endPointUrl comes with /, remove it as action starts with /
+      if (endpointUrl.endsWith("/"))
+        endpointUrl = endpointUrl.substring(0, endpointUrl.length - 1)
+      val appendedReceiverUrl = endpointUrl.concat(actionStr)
+      receiver.put("endpoint_url", appendedReceiverUrl)
+      receiver
+    } else new util.HashMap[String, AnyRef]()
+
+  }
+
+  def fetchDetails(code: String): util.Map[String, AnyRef] = {
+    try {
+      if (registryDataCache.isExists(code)) {
+        Console.println("Getting details from cache for :" + code)
+        val mutableMap = registryDataCache.getWithRetry(code)
+        mutableMap.asJava
+      } else {
+        //Fetch the details from API call
+        Console.println("Could not find the details in cache for code:" + code)
+        val collectionMap = getDetails(code)
+        if (!collectionMap.isEmpty) // Add the registry data into cache if it is not empty
+          registryDataCache.hmSet(code, JSONUtil.serialize(collectionMap))
+        collectionMap
+      }
+    } catch {
+      case ex: Exception =>
+        //In case of issues with redis cache, fetch the details from the registry
+        getDetails(code)
+    }
+  }
+
+  def getDetails(code: String): util.Map[String, AnyRef] = {
+    val responseBody: String = DispatcherUtil.post(config.registryUrl, code)
+    val responseArr = JSONUtil.deserialize[util.ArrayList[util.HashMap[String, AnyRef]]](responseBody)
+    if (!responseArr.isEmpty) {
+      val collectionMap = responseArr.get(0)
+      collectionMap
+    } else {
+      new util.HashMap[String, AnyRef]
+    }
   }
 }
