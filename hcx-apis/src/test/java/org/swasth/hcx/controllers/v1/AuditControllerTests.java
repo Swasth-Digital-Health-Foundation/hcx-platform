7f014f919dee17c4b747caccbcc35c45c2933cde
saurabh-singh-17 <saurabhvikashsingh@gmail.com>
2022-01-17 13:44:33 +0530

    removed auto indexing

---
+++ hcx-apis/src/test/java/org/swasth/hcx/controllers/v1/AuditControllerTests.java
@@ -0,0 +1,44 @@
+package org.swasth.hcx.controllers.v1;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.swasth.common.utils.JSONUtils;
+import org.swasth.common.dto.SearchRequestDTO;
+import org.swasth.hcx.controllers.BaseSpec;
+
+public class AuditControllerTests extends BaseSpec{
+
+    @Test
+    public void audit_search_success_scenario() throws Exception {
+        String uri = "/v1/audit/search";
+        SearchRequestDTO searchrequest = new SearchRequestDTO();
+        HashMap<String,String> filters = new HashMap<String, String>();
+        filters.put("status","submitted");
+        searchrequest.setFilters(filters);
+        searchrequest.setLimit(10);
+        searchrequest.setOffset(0);
+        MvcResult mvcResult =  mockMvc.perform(post(uri).content(JSONUtils.serialize(searchrequest)).contentType(MediaType.APPLICATION_JSON)).andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        int status = response.getStatus();
+        assertEquals(200, status);
+    }
+}
