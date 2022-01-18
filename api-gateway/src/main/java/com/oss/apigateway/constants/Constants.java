bb890928064742c96c70d7708d6f29506ac91fce
Shiva Rakshith <rakshiths@sanketika.in>
2022-01-18 14:40:06 +0530

    feat: add v0.7 protocol header changes

--- api-gateway/src/main/java/com/oss/apigateway/constants/Constants.java
+++ api-gateway/src/main/java/com/oss/apigateway/constants/Constants.java
@@ -5,7 +5,7 @@ public class Constants {
     public static final String X_JWT_SUB_HEADER = "X-jwt-sub";
     public static final String CORRELATION_ID = "x-hcx-correlation_id";
     public static final String WORKFLOW_ID = "x-hcx-workflow_id";
-    public static final String REQUEST_ID = "x-hcx-request_id";
+    public static final String API_CALL_ID = "x-hcx-api_call_id";
     public static final String AUTHORIZATION = "Authorization";
     public static final String PAYLOAD = "payload";
     public static final String SENDER_CODE = "x-hcx-sender_code";
