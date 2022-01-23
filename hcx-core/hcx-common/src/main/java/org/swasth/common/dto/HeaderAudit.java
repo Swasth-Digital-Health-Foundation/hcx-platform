e10abf110f3eb268f6cd5c625d44f559cf63f3af
saurabh-singh-17 <saurabhvikashsingh@gmail.com>
2022-01-23 19:50:00 +0530

    Revert "dto changed for elastic search"
    This reverts commit 098e46303fd4d87c796f9209994ea82cf6d77426.

--- hcx-core/hcx-common/src/main/java/org/swasth/common/dto/HeaderAudit.java
+++ hcx-core/hcx-common/src/main/java/org/swasth/common/dto/HeaderAudit.java
@@ -1,162 +1,84 @@
 package org.swasth.common.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
 public class HeaderAudit {
-
-	private String eid;
-	private Object error_details;
-	private Object debug_details;
-	@JsonProperty("x-hcx-recipient_code")
+	
+	private String request_id;
 	private String recipient_code;
-
-	@JsonProperty("x-hcx-sender_code")
-	private String sender_code;
-
-	@JsonProperty("x-hcx-api_call_id")
-	private String api_call_id;
-
-	@JsonProperty("x-hcx-workflow_id")
-	private String workflow_id;
-
-	@JsonProperty("x-hcx-correlation_id")
 	private String correlation_id;
-
-	@JsonProperty("x-hcx-timestamp")
+	private String workflow_id;
 	private String timestamp;
-
-	private Number requestTimeStamp;
-
-	private Number auditTimestamp;
-
-	private Number updatedTimestamp;
-
-	private String action;
-
+	private String sender_code;
 	private String mid;
+	private String action;
+	private Object log_details;
+	private Object jose;
+	private Object status;
 
-	private String status;
-
-	public String getEid() {
-		return eid;
+	
+	public Object getStatus() {
+		return status;
 	}
-
-	public void setEid(String eid) {
-		this.eid = eid;
+	public void setStatus(Object status) {
+		this.status = status;
 	}
-
-	public Object getError_details() {
-		return error_details;
+	public String getRequest_id() {
+		return request_id;
 	}
-
-	public void setError_details(Object error_details) {
-		this.error_details = error_details;
+	public void setRequest_id(String request_id) {
+		this.request_id = request_id;
 	}
-
-	public Object getDebug_details() {
-		return debug_details;
-	}
-
-	public void setDebug_details(Object debug_details) {
-		this.debug_details = debug_details;
-	}
-
 	public String getRecipient_code() {
 		return recipient_code;
 	}
-
 	public void setRecipient_code(String recipient_code) {
 		this.recipient_code = recipient_code;
 	}
-
-	public String getSender_code() {
-		return sender_code;
-	}
-
-	public void setSender_code(String sender_code) {
-		this.sender_code = sender_code;
-	}
-
-	public String getApi_call_id() {
-		return api_call_id;
-	}
-
-	public void setApi_call_id(String api_call_id) {
-		this.api_call_id = api_call_id;
-	}
-
-	public String getWorkflow_id() {
-		return workflow_id;
-	}
-
-	public void setWorkflow_id(String workflow_id) {
-		this.workflow_id = workflow_id;
-	}
-
 	public String getCorrelation_id() {
 		return correlation_id;
 	}
-
 	public void setCorrelation_id(String correlation_id) {
 		this.correlation_id = correlation_id;
 	}
-
+	public String getWorkflow_id() {
+		return workflow_id;
+	}
+	public void setWorkflow_id(String workflow_id) {
+		this.workflow_id = workflow_id;
+	}
 	public String getTimestamp() {
 		return timestamp;
 	}
-
 	public void setTimestamp(String timestamp) {
 		this.timestamp = timestamp;
 	}
-
-	public Number getRequestTimeStamp() {
-		return requestTimeStamp;
+	public String getSender_code() {
+		return sender_code;
 	}
-
-	public void setRequestTimeStamp(Number requestTimeStamp) {
-		this.requestTimeStamp = requestTimeStamp;
+	public void setSender_code(String sender_code) {
+		this.sender_code = sender_code;
 	}
-
-	public Number getAuditTimestamp() {
-		return auditTimestamp;
-	}
-
-	public void setAuditTimestamp(Number auditTimestamp) {
-		this.auditTimestamp = auditTimestamp;
-	}
-
-	public Number getUpdatedTimestamp() {
-		return updatedTimestamp;
-	}
-
-	public void setUpdatedTimestamp(Number updatedTimestamp) {
-		this.updatedTimestamp = updatedTimestamp;
-	}
-
-	public String getAction() {
-		return action;
-	}
-
-	public void setAction(String action) {
-		this.action = action;
-	}
-
 	public String getMid() {
 		return mid;
 	}
-
 	public void setMid(String mid) {
 		this.mid = mid;
 	}
-
-	public String getStatus() {
-		return status;
+	public String getAction() {
+		return action;
 	}
-
-	public void setStatus(String status) {
-		this.status = status;
+	public void setAction(String action) {
+		this.action = action;
 	}
-
-
-
+	public Object getLog_details() {
+		return log_details;
+	}
+	public void setLog_details(Object log_details) {
+		this.log_details = log_details;
+	}
+	public Object getJose() {
+		return jose;
+	}
+	public void setJose(Object jose) {
+		this.jose = jose;
+	}
 }
