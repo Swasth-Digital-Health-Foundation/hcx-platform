c939a5443d09f7e6ed71a6d86aabf732468f450d
Shiva Rakshith <rakshiths@sanketika.in>
2022-02-16 13:55:49 +0530

    Revert "feat: add audit service and refactor controllers"
    This reverts commit ab46910701d7027032de1d8fbab52e25a2afadaa.

--- api-gateway/src/main/java/org/swasth/apigateway/constants/Constants.java
+++ api-gateway/src/main/java/org/swasth/apigateway/constants/Constants.java
@@ -3,54 +3,54 @@ package org.swasth.apigateway.constants;
 import java.util.Arrays;
 import java.util.List;
 
-public interface Constants {
+public class Constants {
 
-    String VERSION_PREFIX = "/v1";
+    public static final String VERSION_PREFIX = "/v1";
 
     // Coverage Eligibility APIs
-    String COVERAGE_ELIGIBILITY_CHECK = VERSION_PREFIX + "/coverageeligibility/check";
-    String COVERAGE_ELIGIBILITY_ONCHECK = VERSION_PREFIX + "/coverageeligibility/on_check";
+    public static final String COVERAGE_ELIGIBILITY_CHECK = VERSION_PREFIX + "/coverageeligibility/check";
+    public static final String COVERAGE_ELIGIBILITY_ONCHECK = VERSION_PREFIX + "/coverageeligibility/on_check";
 
     // Claims APIs
-    String PRE_AUTH_SUBMIT = VERSION_PREFIX + "/preauth/submit";
-    String PRE_AUTH_ONSUBMIT = VERSION_PREFIX + "/preauth/on_submit";
-    String CLAIM_SUBMIT = VERSION_PREFIX + "/claim/submit";
-    String CLAIM_ONSUBMIT = VERSION_PREFIX + "/claim/on_submit";
+    public static final String PRE_AUTH_SUBMIT = VERSION_PREFIX + "/preauth/submit";
+    public static final String PRE_AUTH_ONSUBMIT = VERSION_PREFIX + "/preauth/on_submit";
+    public static final String CLAIM_SUBMIT = VERSION_PREFIX + "/claim/submit";
+    public static final String CLAIM_ONSUBMIT = VERSION_PREFIX + "/claim/on_submit";
 
     //Payment Notice APIs
-    String PAYMENT_NOTICE_REQUEST = VERSION_PREFIX + "/paymentnotice/request";
-    String PAYMENT_NOTICE_ONREQUEST = VERSION_PREFIX + "/paymentnotice/on_request";
+    public static final String PAYMENT_NOTICE_REQUEST = VERSION_PREFIX + "/paymentnotice/request";
+    public static final String PAYMENT_NOTICE_ONREQUEST = VERSION_PREFIX + "/paymentnotice/on_request";
 
     //Status Search APIs
-    String HCX_STATUS = VERSION_PREFIX + "/hcx/status";
-    String HCX_ONSTATUS = VERSION_PREFIX + "/hcx/on_status";
+    public static final String HCX_STATUS = VERSION_PREFIX + "/hcx/status";
+    public static final String HCX_ONSTATUS = VERSION_PREFIX + "/hcx/on_status";
 
     // Search APIs
-    String HCX_SEARCH = VERSION_PREFIX + "/hcx/search";
-    String HCX_ON_SEARCH = VERSION_PREFIX + "/hcx/on_search";
+    public static final String HCX_SEARCH = VERSION_PREFIX + "/hcx/search";
+    public static final String HCX_ON_SEARCH = VERSION_PREFIX + "/hcx/on_search";
 
-    List<String> ON_ACTION_APIS = Arrays.asList(COVERAGE_ELIGIBILITY_ONCHECK, PRE_AUTH_ONSUBMIT, CLAIM_ONSUBMIT, PAYMENT_NOTICE_ONREQUEST, HCX_ONSTATUS, HCX_ON_SEARCH);
+    public static final List<String> ON_ACTION_APIS = Arrays.asList(COVERAGE_ELIGIBILITY_ONCHECK, PRE_AUTH_ONSUBMIT, CLAIM_ONSUBMIT, PAYMENT_NOTICE_ONREQUEST, HCX_ONSTATUS, HCX_ON_SEARCH);
 
-    int PAYLOAD_LENGTH = 5;
-    String AUTH_REQUIRED = "AUTH_REQUIRED";
-    String X_JWT_SUB_HEADER = "X-jwt-sub";
-    String CORRELATION_ID = "x-hcx-correlation_id";
-    String WORKFLOW_ID = "x-hcx-workflow_id";
-    String API_CALL_ID = "x-hcx-api_call_id";
-    String AUTHORIZATION = "Authorization";
-    String PAYLOAD = "payload";
-    String SENDER_CODE = "x-hcx-sender_code";
-    String RECIPIENT_CODE = "x-hcx-recipient_code";
-    String TIMESTAMP = "x-hcx-timestamp";
-    String DEBUG_FLAG = "x-hcx-debug_flag";
-    List<String> DEBUG_FLAG_VALUES = Arrays.asList("Error","Info","Debug");
-    String STATUS = "x-hcx-status";
-    List<String> STATUS_VALUES = Arrays.asList("request.queued", "request.dispatched", "response.complete", "response.partial", "response.error", "response.redirect");
-    String ERROR_DETAILS = "x-hcx-error_details";
-    List<String> ERROR_DETAILS_VALUES = Arrays.asList("code","message","trace");
-    String DEBUG_DETAILS = "x-hcx-debug_details";
-    String BLOCKED = "Blocked";
-    String INACTIVE = "Inactive";
-    String OS_OWNER = "osOwner";
+    public static final int PAYLOAD_LENGTH = 5;
+    public static final String AUTH_REQUIRED = "AUTH_REQUIRED";
+    public static final String X_JWT_SUB_HEADER = "X-jwt-sub";
+    public static final String CORRELATION_ID = "x-hcx-correlation_id";
+    public static final String WORKFLOW_ID = "x-hcx-workflow_id";
+    public static final String API_CALL_ID = "x-hcx-api_call_id";
+    public static final String AUTHORIZATION = "Authorization";
+    public static final String PAYLOAD = "payload";
+    public static final String SENDER_CODE = "x-hcx-sender_code";
+    public static final String RECIPIENT_CODE = "x-hcx-recipient_code";
+    public static final String TIMESTAMP = "x-hcx-timestamp";
+    public static final String DEBUG_FLAG = "x-hcx-debug_flag";
+    public static final List<String> DEBUG_FLAG_VALUES = Arrays.asList("Error","Info","Debug");
+    public static final String STATUS = "x-hcx-status";
+    public static final List<String> STATUS_VALUES = Arrays.asList("request.queued", "request.dispatched", "response.complete", "response.partial", "response.error", "response.redirect");
+    public static final String ERROR_DETAILS = "x-hcx-error_details";
+    public static final List<String> ERROR_DETAILS_VALUES = Arrays.asList("code","message","trace");
+    public static final String DEBUG_DETAILS = "x-hcx-debug_details";
+    public static final String BLOCKED = "Blocked";
+    public static final String INACTIVE = "Inactive";
+    public static final String OS_OWNER = "osOwner";
 
 }
