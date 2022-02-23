e5e9d81da7ca0b70af889e6c8f9ab169fb4668aa
Prasad Moka <prasad@sanketika.in>
2022-02-23 11:40:04 +0530

    fix: redirect testing issues

--- api-gateway/src/main/java/org/swasth/apigateway/models/JSONRequest.java
+++ api-gateway/src/main/java/org/swasth/apigateway/models/JSONRequest.java
@@ -16,19 +16,19 @@ public class JSONRequest extends BaseRequest{
         super(payload,isJSONRequest,apiAction);
     }
 
-    public void validateRedirectRequest(List<String> allowedRoles,Map<String, Object> redirectDetails,List<Object> callAuditData,List<Object> correlationAuditData) throws Exception {
-            validateCondition(StringUtils.isEmpty(getRedirectTo()),ErrorCodes.ERR_INVALID_REDIRECT_TO, "Redirect requests must have valid participant code for field "+REDIRECT_TO);
+    public void validateRedirect(List<String> allowedRoles,Map<String, Object> redirectDetails,List<Object> callAuditData,List<Object> correlationAuditData) throws Exception {
+            validateCondition(StringUtils.isEmpty(getRedirectTo()), ErrorCodes.ERR_INVALID_REDIRECT_TO, "Redirect requests must have valid participant code for field " + REDIRECT_TO);
             validateCondition(getSenderCode().equalsIgnoreCase(getRedirectTo()), ErrorCodes.ERR_INVALID_REDIRECT_TO, "Sender can not redirect request to self");
 
             validateParticipant(redirectDetails, ErrorCodes.ERR_INVALID_REDIRECT_TO, "Redirected");
             List<String> redirectRoles = (List<String>) redirectDetails.get(ROLES);
-            validateCondition(!hasRedirectRole(allowedRoles, redirectRoles), ErrorCodes.ERR_INVALID_REDIRECT_TO, "Redirected participant do not have access to send across on_* API calls");
+            validateCondition(!hasRedirectRole(allowedRoles, redirectRoles), ErrorCodes.ERR_INVALID_REDIRECT_TO, "Redirected participant do not have access to send across callbacks (on_* API calls)");
 
             validateCondition(!callAuditData.isEmpty(), ErrorCodes.ERR_INVALID_API_CALL_ID, "Already request exists with same api call id:" + getApiCallId());
             validateCondition(correlationAuditData.isEmpty(), ErrorCodes.ERR_INVALID_CORRELATION_ID, "The on_action request should contain the same correlation id as in corresponding action request");
             Map<String, Object> auditEvent = (Map<String, Object>) correlationAuditData.get(0);
             if (auditEvent.containsKey(WORKFLOW_ID)) {
-                validateCondition(!getWorkflowId().equals(auditEvent.get(WORKFLOW_ID)), ErrorCodes.ERR_INVALID_WORKFLOW_ID, "The on_action request should contain the same workflow id as in corresponding action request");
+                validateCondition(!getProtocolHeaders().containsKey(WORKFLOW_ID) || !getWorkflowId().equals(auditEvent.get(WORKFLOW_ID)), ErrorCodes.ERR_INVALID_WORKFLOW_ID, "The on_action request should contain the same workflow id as in corresponding action request");
             }
 
             for (Object audit : correlationAuditData) {
@@ -36,8 +36,8 @@ public class JSONRequest extends BaseRequest{
                 //Check for only on_* requests from Audit data
                 if (((String) auditEventData.get(ACTION)).contains("on_")) {
                     if (auditEventData.containsKey(STATUS)) {
-                        //Identifying open redirect requests
-                        validateCondition(REDIRECT_STATUS.equals(auditEvent.get(STATUS)), ErrorCodes.ERR_INVALID_REDIRECT_TO, "The redirect request has been closed with status as complete");
+                        //Identifying any complete responses
+                        validateCondition(COMPLETE_STATUS.equals(auditEvent.get(STATUS)), ErrorCodes.ERR_INVALID_REDIRECT_TO, "The redirected request has been closed with status as response.complete");
                     }
                     //validate the redirected has any one of request initiators
                     validateCondition(getRedirectTo().equalsIgnoreCase((String) auditEventData.get(SENDER_CODE)), ErrorCodes.ERR_INVALID_REDIRECT_TO, "Redirect request can not be redirected to one of the initiators");
--- hcx-apis/src/main/java/org/swasth/hcx/helpers/EventGenerator.java
+++ hcx-apis/src/main/java/org/swasth/hcx/helpers/EventGenerator.java
@@ -18,7 +18,6 @@ public class EventGenerator {
 
     @Autowired
     Environment env;
-
     public String generatePayloadEvent(String mid, Request request) throws JsonProcessingException {
         Map<String,Object> event = new HashMap<>();
         event.put(MID, mid);
@@ -28,33 +27,52 @@ public class EventGenerator {
 
     public String generateMetadataEvent(String mid, String apiAction, Request request) throws Exception {
         Map<String,Object> event = new HashMap<>();
-        List<String> protocolHeaders = env.getProperty(PROTOCOL_HEADERS_MANDATORY, List.class);
-        protocolHeaders.addAll(env.getProperty(PROTOCOL_HEADERS_OPTIONAL, List.class));
-        List<String> joseHeaders = env.getProperty(JOSE_HEADERS, List.class);
-        Map<String,Object> protectedHeaders = request.getHcxHeaders();
-        Map<String,Object> filterJoseHeaders = new HashMap<>();
-        Map<String,Object> filterProtocolHeaders = new HashMap<>();
-        joseHeaders.forEach(key -> {
-            if (protectedHeaders.containsKey(key))
-                filterJoseHeaders.put(key, protectedHeaders.get(key));
-        });
-        protocolHeaders.forEach(key -> {
-            if (protectedHeaders.containsKey(key))
-                filterProtocolHeaders.put(key, protectedHeaders.get(key));
-        });
-        event.put(MID, mid);
-        event.put(ETS, System.currentTimeMillis());
-        event.put(ACTION, apiAction);
-        event.put(HEADERS, new HashMap<>(){{
-            put(JOSE, filterJoseHeaders);
-            put(PROTOCOL, filterProtocolHeaders);
-        }});
-        event.put(LOG_DETAILS, new HashMap<>(){{
-            put(CODE, "");
-            put(MESSAGE, "");
-            put(TRACE, "");
-        }});
-        event.put("status", "request.queued");
+        List<String> protocolHeaders = null;
+        if (request.getPayload().containsKey(PAYLOAD)) {
+            protocolHeaders = env.getProperty(PROTOCOL_HEADERS_MANDATORY, List.class);
+            protocolHeaders.addAll(env.getProperty(PROTOCOL_HEADERS_OPTIONAL, List.class));
+            List<String> joseHeaders = env.getProperty(JOSE_HEADERS, List.class);
+            Map<String,Object> protectedHeaders = request.getHcxHeaders();
+            Map<String,Object> filterJoseHeaders = new HashMap<>();
+            Map<String,Object> filterProtocolHeaders = new HashMap<>();
+            joseHeaders.forEach(key -> {
+                if (protectedHeaders.containsKey(key))
+                    filterJoseHeaders.put(key, protectedHeaders.get(key));
+            });
+            protocolHeaders.forEach(key -> {
+                if (protectedHeaders.containsKey(key))
+                    filterProtocolHeaders.put(key, protectedHeaders.get(key));
+            });
+            event.put(MID, mid);
+            event.put(ETS, System.currentTimeMillis());
+            event.put(ACTION, apiAction);
+            event.put(HEADERS, new HashMap<>(){{
+                put(JOSE, filterJoseHeaders);
+                put(PROTOCOL, filterProtocolHeaders);
+            }});
+            event.put("status", "request.queued");
+        } else {
+            if(REDIRECT_STATUS.equalsIgnoreCase(request.getStatus())) {
+                protocolHeaders = env.getProperty(REDIRECT_HEADERS_MANDATORY, List.class);
+                protocolHeaders.addAll(env.getProperty(REDIRECT_HEADERS_OPTIONAL, List.class));
+            }else {
+                protocolHeaders = env.getProperty(ERROR_HEADERS_MANDATORY, List.class);
+                protocolHeaders.addAll(env.getProperty(ERROR_HEADERS_OPTIONAL, List.class));
+            }
+            Map<String, Object> protectedHeaders = request.getHcxHeaders();
+            Map<String, Object> filterProtocolHeaders = new HashMap<>();
+            protocolHeaders.forEach(key -> {
+                if (protectedHeaders.containsKey(key))
+                    filterProtocolHeaders.put(key, protectedHeaders.get(key));
+            });
+            event.put(HEADERS, new HashMap<>() {{
+                put(PROTOCOL, filterProtocolHeaders);
+            }});
+            event.put(MID, mid);
+            event.put(ETS, System.currentTimeMillis());
+            event.put(ACTION, apiAction);
+            event.put("status", "request.queued");
+        }
         return JSONUtils.serialize(event);
     }
 }
