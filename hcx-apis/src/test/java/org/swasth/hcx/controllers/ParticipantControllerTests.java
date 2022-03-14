72d078d7e09fdb625617b47a3de16067ef61e798
Dharmateja <dharmateja.nandipattu@tarento.com>
2022-03-14 11:48:36 +0530

    feat: HCX-67- improve code coverage

--- hcx-apis/src/test/java/org/swasth/hcx/controllers/ParticipantControllerTests.java
+++ hcx-apis/src/test/java/org/swasth/hcx/controllers/ParticipantControllerTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
-public class ParticipantControllerTests extends BaseSpec{
+class ParticipantControllerTests extends BaseSpec{
 
 
 
