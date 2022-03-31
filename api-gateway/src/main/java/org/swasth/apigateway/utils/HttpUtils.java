981bd8f61393e9c52d90e4311faf08149e6be1e6
Shiva Rakshith <rakshiths@sanketika.in>
2022-03-31 11:20:23 +0530

    feat: update httputils

--- api-gateway/src/main/java/org/swasth/apigateway/utils/HttpUtils.java
+++ api-gateway/src/main/java/org/swasth/apigateway/utils/HttpUtils.java
@@ -7,10 +7,6 @@ import java.util.Collections;
 
 public class HttpUtils {
 
-    public static HttpResponse get(String url) {
-        return Unirest.get(url).headers(Collections.singletonMap("Content-Type","application/json")).asString();
-    }
-
     public static HttpResponse post(String url, String requestBody){
         return Unirest.post(url).headers(Collections.singletonMap("Content-Type","application/json")).body(requestBody).asString();
     }
