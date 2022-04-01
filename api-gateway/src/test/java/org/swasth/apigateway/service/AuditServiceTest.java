64377cb96a5b9155d42fbe75d08374af43234995
Shiva Rakshith <rakshiths@sanketika.in>
2022-04-01 16:01:02 +0530

    Update AuditServiceTest.java

--- api-gateway/src/test/java/org/swasth/apigateway/service/AuditServiceTest.java
+++ api-gateway/src/test/java/org/swasth/apigateway/service/AuditServiceTest.java
@@ -9,11 +9,10 @@ import org.swasth.apigateway.BaseSpec;
 import org.swasth.apigateway.constants.Constants;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
@@ -21,7 +20,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 public class AuditServiceTest extends BaseSpec {
 
     @Test
-    public void check_audit_service_success_scenario() throws Exception {
+    public void check_audit_server_success_scenario() throws Exception {
         server.enqueue(new MockResponse()
                 .setResponseCode(200)
                 .setBody("[{\"test\":\"123\"}]")
@@ -29,26 +28,10 @@ public class AuditServiceTest extends BaseSpec {
 
         Mockito.when(auditService.getAuditLogs(any())).thenCallRealMethod();
         ReflectionTestUtils.setField(auditService, "hcxApiUrl", "http://localhost:8080");
-        List<Map<String, Object>> result = auditService.getAuditLogs(Collections.singletonMap("x-hcx-correlation_id", "5e934f90-111d-4f0b-b016-c22d820674e1"));
-        assertFalse(result.isEmpty());
-    }
-
-    @Test
-    public void check_audit_service_server_exception_scenario() throws Exception {
-        Mockito.when(registryService.fetchDetails(anyString(), anyString()))
-                .thenReturn(getProviderDetails())
-                .thenReturn(getPayorDetails());
-        Mockito.when(auditService.getAuditLogs(any())).thenCallRealMethod();
-
-        client.post().uri("/v1/coverageeligibility/check")
-                .header(Constants.AUTHORIZATION, getProviderToken())
-                .header("X-jwt-sub", "f7c0e759-bec3-431b-8c4f-6b294d103a74")
-                .bodyValue(getRequestBody())
-                .exchange()
-                .expectBody(Map.class)
-                .consumeWith(result -> {
-                    assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatus());
-                });
+        Exception exception = assertThrows(Exception.class, () -> {
+            auditService.getAuditLogs(Collections.singletonMap("x-hcx-correlation_id", "5e934f90-111d-4f0b-b016-c22d820674e1"));
+        });
+        assertEquals("Error connecting to audit service: org.apache.http.NoHttpResponseException: localhost:8080 failed to respond", exception.getMessage());
     }
 
     @Test
