cd919f5fd77a2d444f1ead3bc2501dab3dbd5b69
Shiva Rakshith <rakshiths@sanketika.in>
2022-04-01 16:18:05 +0530

    Revert "Update RegistryServiceTest.java"
    This reverts commit a640610cb2d99156fc9b6fd643953ea2528349ec.

--- api-gateway/src/test/java/org/swasth/apigateway/service/RegistryServiceTest.java
+++ api-gateway/src/test/java/org/swasth/apigateway/service/RegistryServiceTest.java
@@ -13,21 +13,6 @@ import static org.mockito.ArgumentMatchers.any;
 public class RegistryServiceTest extends BaseSpec {
 
     @Test
-    public void check_registry_service_server_exception_scenario() throws Exception {
-        server.enqueue(new MockResponse()
-                .setResponseCode(200)
-                .setBody("[{\"test\":\"123\"}]")
-                .addHeader("Content-Type", "application/json"));
-
-        Mockito.when(registryService.getDetails(any(), any())).thenCallRealMethod();
-        ReflectionTestUtils.setField(registryService, "registryUrl", "http://localhost:8080");
-        Exception exception = assertThrows(Exception.class, () -> {
-            registryService.getDetails("osid", "1-5e934f90-111d-4f0b-b016-c22d820674e1");
-        });
-        assertEquals("Error connecting to registry service: org.apache.http.NoHttpResponseException: localhost:8080 failed to respond", exception.getMessage());
-    }
-
-    @Test
     public void check_registry_service_internal_server_exception_scenario() throws Exception {
         server.enqueue(new MockResponse()
                 .setResponseCode(400)
