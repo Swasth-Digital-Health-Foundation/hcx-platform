f83a88af36d29762a46b786ffb0b0be0ba95f343
Shiva Rakshith <rakshiths@sanketika.in>
2022-04-01 15:51:25 +0530

    Update AuditServiceTest.java

--- api-gateway/src/test/java/org/swasth/apigateway/service/AuditServiceTest.java
+++ api-gateway/src/test/java/org/swasth/apigateway/service/AuditServiceTest.java
@@ -11,8 +11,7 @@ import org.swasth.apigateway.constants.Constants;
 import java.util.Collections;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 
