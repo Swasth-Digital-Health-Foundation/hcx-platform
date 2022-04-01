889b8d94c6e7cee81ec723c6d2252d131fcea409
PrasadMoka <prasad@sanketika.in>
2022-04-01 20:40:24 +0530

    HCX-112:Status value changes for on_ API Calls

--- api-gateway/src/test/java/org/swasth/apigateway/filters/HCXRequestTest.java
+++ api-gateway/src/test/java/org/swasth/apigateway/filters/HCXRequestTest.java
@@ -280,15 +280,15 @@ public class HCXRequestTest extends BaseSpec {
                 .thenReturn(getProviderDetails())
                 .thenReturn(getPayorDetails());
 
-        client.post().uri("/v1/coverageeligibility/check")
+        client.post().uri("/v1/coverageeligibility/on_check")
                 .header(Constants.AUTHORIZATION, getProviderToken())
                 .header("X-jwt-sub", "f7c0e759-bec3-431b-8c4f-6b294d103a74")
-                .bodyValue("{ \"x-hcx-status\": \"request.queued\", \"x-hcx-sender_code\": \"1-30b6ac38-bbfe-4012-9d9c-dcbe8420f68f\", \"x-hcx-recipient_code\": \"1-281cca66-55cd-4c1b-96bb-51813cdbbe17\", \"x-hcx-correlation_id\":\"5e934f90-111d-4f0b-b016-c22d820674e4\", \"x-hcx-api_call_id\": \"24aee489-d0d5-47b6-b3c1-559732c4b385\", \"x-hcx-timestamp\": \"2021-10-27T20:35:52.636+0530\", \"x-hcx-error_details\": { } }")
+                .bodyValue("{ \"x-hcx-status\": \"response.error\", \"x-hcx-sender_code\": \"1-30b6ac38-bbfe-4012-9d9c-dcbe8420f68f\", \"x-hcx-recipient_code\": \"1-281cca66-55cd-4c1b-96bb-51813cdbbe17\", \"x-hcx-correlation_id\":\"5e934f90-111d-4f0b-b016-c22d820674e4\", \"x-hcx-api_call_id\": \"24aee489-d0d5-47b6-b3c1-559732c4b385\", \"x-hcx-timestamp\": \"2021-10-27T20:35:52.636+0530\", \"x-hcx-error_details\": { } }")
                 .exchange()
                 .expectBody(Map.class)
                 .consumeWith(result -> {
                     assertEquals(HttpStatus.BAD_REQUEST, result.getStatus());
-                    assertEquals(ErrorCodes.ERR_INVALID_PAYLOAD.name(), getResponseErrorCode(result));
+                    assertEquals(ErrorCodes.ERR_INVALID_ERROR_DETAILS.name(), getResponseErrorCode(result));
                 });
     }
 
@@ -298,15 +298,15 @@ public class HCXRequestTest extends BaseSpec {
                 .thenReturn(getProviderDetails())
                 .thenReturn(getPayorDetails());
 
-        client.post().uri("/v1/coverageeligibility/check")
+        client.post().uri("/v1/coverageeligibility/on_check")
                 .header(Constants.AUTHORIZATION, getProviderToken())
                 .header("X-jwt-sub", "f7c0e759-bec3-431b-8c4f-6b294d103a74")
-                .bodyValue("{ \"x-hcx-status\": \"request.queued\", \"x-hcx-sender_code\": \"1-30b6ac38-bbfe-4012-9d9c-dcbe8420f68f\", \"x-hcx-recipient_code\": \"1-281cca66-55cd-4c1b-96bb-51813cdbbe17\", \"x-hcx-correlation_id\":\"5e934f90-111d-4f0b-b016-c22d820674e4\", \"x-hcx-api_call_id\": \"24aee489-d0d5-47b6-b3c1-559732c4b385\", \"x-hcx-timestamp\": \"2021-10-27T20:35:52.636+0530\", \"x-hcx-error_details\": {\"message\": \"\", \"trace\": \"Recipient Invalid Encryption\" } }")
+                .bodyValue("{ \"x-hcx-status\": \"response.error\", \"x-hcx-sender_code\": \"1-30b6ac38-bbfe-4012-9d9c-dcbe8420f68f\", \"x-hcx-recipient_code\": \"1-281cca66-55cd-4c1b-96bb-51813cdbbe17\", \"x-hcx-correlation_id\":\"5e934f90-111d-4f0b-b016-c22d820674e4\", \"x-hcx-api_call_id\": \"24aee489-d0d5-47b6-b3c1-559732c4b385\", \"x-hcx-timestamp\": \"2021-10-27T20:35:52.636+0530\", \"x-hcx-error_details\": {\"message\": \"\", \"trace\": \"Recipient Invalid Encryption\" } }")
                 .exchange()
                 .expectBody(Map.class)
                 .consumeWith(result -> {
                     assertEquals(HttpStatus.BAD_REQUEST, result.getStatus());
-                    assertEquals(ErrorCodes.ERR_INVALID_PAYLOAD.name(), getResponseErrorCode(result));
+                    assertEquals(ErrorCodes.ERR_INVALID_ERROR_DETAILS.name(), getResponseErrorCode(result));
                 });
     }
 
@@ -316,15 +316,15 @@ public class HCXRequestTest extends BaseSpec {
                 .thenReturn(getProviderDetails())
                 .thenReturn(getPayorDetails());
 
-        client.post().uri("/v1/coverageeligibility/check")
+        client.post().uri("/v1/coverageeligibility/on_check")
                 .header(Constants.AUTHORIZATION, getProviderToken())
                 .header("X-jwt-sub", "f7c0e759-bec3-431b-8c4f-6b294d103a74")
-                .bodyValue("{ \"x-hcx-status\": \"request.queued\", \"x-hcx-sender_code\": \"1-30b6ac38-bbfe-4012-9d9c-dcbe8420f68f\", \"x-hcx-recipient_code\": \"1-281cca66-55cd-4c1b-96bb-51813cdbbe17\", \"x-hcx-correlation_id\":\"5e934f90-111d-4f0b-b016-c22d820674e4\", \"x-hcx-api_call_id\": \"24aee489-d0d5-47b6-b3c1-559732c4b385\", \"x-hcx-timestamp\": \"2021-10-27T20:35:52.636+0530\", \"x-hcx-error_details\": { \"code\": \"ERR_INVALID_ENCRYPTION\", \"message\": \"\", \"trace\": \"Recipient Invalid Encryption\", \"test\": \"\"} }")
+                .bodyValue("{ \"x-hcx-status\": \"response.error\", \"x-hcx-sender_code\": \"1-30b6ac38-bbfe-4012-9d9c-dcbe8420f68f\", \"x-hcx-recipient_code\": \"1-281cca66-55cd-4c1b-96bb-51813cdbbe17\", \"x-hcx-correlation_id\":\"5e934f90-111d-4f0b-b016-c22d820674e4\", \"x-hcx-api_call_id\": \"24aee489-d0d5-47b6-b3c1-559732c4b385\", \"x-hcx-timestamp\": \"2021-10-27T20:35:52.636+0530\", \"x-hcx-error_details\": { \"code\": \"ERR_INVALID_ENCRYPTION\", \"message\": \"\", \"trace\": \"Recipient Invalid Encryption\", \"test\": \"\"} }")
                 .exchange()
                 .expectBody(Map.class)
                 .consumeWith(result -> {
                     assertEquals(HttpStatus.BAD_REQUEST, result.getStatus());
-                    assertEquals(ErrorCodes.ERR_INVALID_PAYLOAD.name(), getResponseErrorCode(result));
+                    assertEquals(ErrorCodes.ERR_INVALID_ERROR_DETAILS.name(), getResponseErrorCode(result));
                 });
     }
 
