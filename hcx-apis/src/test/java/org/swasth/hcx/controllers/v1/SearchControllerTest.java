4845e971ee554cd292c31629f3ca8e52e9732794
Shiva Rakshith <rakshiths@sanketika.in>
2022-04-03 10:56:58 +0530

    feat: add payload to audit event

--- hcx-apis/src/test/java/org/swasth/hcx/controllers/v1/SearchControllerTest.java
+++ hcx-apis/src/test/java/org/swasth/hcx/controllers/v1/SearchControllerTest.java
@@ -58,7 +58,6 @@ public class SearchControllerTest extends BaseSpec {
 
     @Test
     public void onSearchBadRequest() throws Exception {
-        when(headerAuditService.search(any())).thenReturn(Arrays.asList(new HeaderAudit("AUDIT", new Object(), new Object(), "1-2799b6a4-cf2d-45fe-a5e1-5f1c82979e0d", "93f908ba", "ff84928c-a077-4565-8fb1-731b1b6466a0", "", "1e83-460a-4f0b-b016-c22d820674e1", "2022-01-06T09:50:23+00", new Long("1642781095099"), new Long("1642781095099"), new Long("1642781095099"), "/v1/hcx/search", "59cefda2-a4cc-4795-95f3-fb9e82e21cef", "request.queued", Arrays.asList("provider"), Arrays.asList("payor"))));
         doNothing().when(mockKafkaClient).send(anyString(),anyString(),any());
         String requestBody = getOnSearchBadRequest();
         MvcResult mvcResult = mockMvc.perform(post(HCX_ON_SEARCH).content(requestBody).contentType(MediaType.APPLICATION_JSON)).andReturn();
@@ -75,7 +74,6 @@ public class SearchControllerTest extends BaseSpec {
 
     @Test
     public void onSearchSuccess() throws Exception {
-        when(headerAuditService.search(any())).thenReturn(Arrays.asList(new HeaderAudit("AUDIT", new Object(), new Object(), "1-2799b6a4-cf2d-45fe-a5e1-5f1c82979e0d", "93f908ba", "ff84928c-a077-4565-8fb1-731b1b6466a0", "", "1e83-460a-4f0b-b016-c22d820674e1", "2022-01-06T09:50:23+00", new Long("1642781095099"), new Long("1642781095099"), new Long("1642781095099"), "/v1/hcx/search", "59cefda2-a4cc-4795-95f3-fb9e82e21cef", "request.queued", Arrays.asList("provider"), Arrays.asList("payor"))));
         doNothing().when(mockKafkaClient).send(anyString(),anyString(),any());
         String requestBody = getOnSearchRequest();
         MvcResult mvcResult = mockMvc.perform(post(HCX_ON_SEARCH).content(requestBody).contentType(MediaType.APPLICATION_JSON)).andReturn();
