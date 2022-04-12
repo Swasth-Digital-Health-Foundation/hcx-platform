ec9ff1102e7047423f27c6eafbd14e59c37ae459
Shiva Rakshith <rakshiths@sanketika.in>
2022-04-12 15:01:45 +0530

    feat: add test cases

--- hcx-apis/src/test/java/org/swasth/hcx/controllers/v1/StatusTests.java
+++ hcx-apis/src/test/java/org/swasth/hcx/controllers/v1/StatusTests.java
@@ -23,7 +23,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 public class StatusTests extends BaseSpec {
 
     @Test
-    public void status_success_scenario() throws Exception {
+    public void status_success_for_request_dispatched_scenario() throws Exception {
         doNothing().when(mockKafkaClient).send(anyString(),anyString(),any());
         when(headerAuditService.search(any())).thenReturn(Arrays.asList(new HeaderAudit("AUDIT", new Object(), new Object(), "1-2799b6a4-cf2d-45fe-a5e1-5f1c82979e0d", "93f908ba", "26b1060c-1e83-4600-9612-ea31e0ca5091", "1e83-460a-4f0b-b016-c22d820674e1", "5e934f90-111d-4f0b-b016-c22d820674e1", "2022-01-06T09:50:23+00", new Long("1642781095099"), new Long("1642781095099"), new Long("1642781095099"), "/v1/coverageeligibility/check", "200c6dac-b259-4d35-b176-370fb092d7b0", "request.dispatched", Arrays.asList("provider"), Arrays.asList("payor"), "test_payload")));
         String requestBody = getStatusRequestBody();
@@ -31,6 +31,43 @@ public class StatusTests extends BaseSpec {
         MockHttpServletResponse response = mvcResult.getResponse();
         int status = response.getStatus();
         assertEquals(202, status);
+    }
+
+    @Test
+    public void status_success_for_request_queued_scenario() throws Exception {
+        doNothing().when(mockKafkaClient).send(anyString(),anyString(),any());
+        when(headerAuditService.search(any())).thenReturn(Arrays.asList(new HeaderAudit("AUDIT", new Object(), new Object(), "1-2799b6a4-cf2d-45fe-a5e1-5f1c82979e0d", "93f908ba", "26b1060c-1e83-4600-9612-ea31e0ca5091", "1e83-460a-4f0b-b016-c22d820674e1", "5e934f90-111d-4f0b-b016-c22d820674e1", "2022-01-06T09:50:23+00", new Long("1642781095099"), new Long("1642781095099"), new Long("1642781095099"), "/v1/coverageeligibility/check", "200c6dac-b259-4d35-b176-370fb092d7b0", "request.queued", Arrays.asList("provider"), Arrays.asList("payor"), "test_payload")));
+        String requestBody = getStatusRequestBody();
+        MvcResult mvcResult = mockMvc.perform(post("/v1/hcx/status").content(requestBody).contentType(MediaType.APPLICATION_JSON)).andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        int status = response.getStatus();
+        assertEquals(202, status);
+    }
+
+    @Test
+    public void status_empty_audit_response_scenario() throws Exception {
+        doNothing().when(mockKafkaClient).send(anyString(),anyString(),any());
+        when(headerAuditService.search(any())).thenReturn(Arrays.asList());
+        String requestBody = getStatusRequestBody();
+        MvcResult mvcResult = mockMvc.perform(post("/v1/hcx/status").content(requestBody).contentType(MediaType.APPLICATION_JSON)).andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        int status = response.getStatus();
+        Map<String,Object> responseBody = JSONUtils.deserialize(response.getContentAsString(), Map.class);
+        assertEquals(400, status);
+        assertEquals("Invalid correlation id, details do not exist", getResponseErrorMessage(responseBody));
+    }
+
+    @Test
+    public void status_invalid_entity_scenario() throws Exception {
+        doNothing().when(mockKafkaClient).send(anyString(),anyString(),any());
+        when(headerAuditService.search(any())).thenReturn(Arrays.asList(new HeaderAudit("AUDIT", new Object(), new Object(), "1-2799b6a4-cf2d-45fe-a5e1-5f1c82979e0d", "93f908ba", "26b1060c-1e83-4600-9612-ea31e0ca5091", "1e83-460a-4f0b-b016-c22d820674e1", "5e934f90-111d-4f0b-b016-c22d820674e1", "2022-01-06T09:50:23+00", new Long("1642781095099"), new Long("1642781095099"), new Long("1642781095099"), "/v1/paymentnotice/request", "200c6dac-b259-4d35-b176-370fb092d7b0", "request.dispatched", Arrays.asList("provider"), Arrays.asList("payor"), "test_payload")));
+        String requestBody = getStatusRequestBody();
+        MvcResult mvcResult = mockMvc.perform(post("/v1/hcx/status").content(requestBody).contentType(MediaType.APPLICATION_JSON)).andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        int status = response.getStatus();
+        Map<String,Object> responseBody = JSONUtils.deserialize(response.getContentAsString(), Map.class);
+        assertEquals(400, status);
+        assertEquals("Invalid entity, status search allowed only for entities: [coverageeligibility, preauth, claim, predetermination]", getResponseErrorMessage(responseBody));
     }
 
     @Test
