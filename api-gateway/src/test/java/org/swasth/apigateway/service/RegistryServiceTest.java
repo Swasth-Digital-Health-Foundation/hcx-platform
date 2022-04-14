ee7c63f37c22f539d10228e6d259234b78e72920
Shiva Rakshith <rakshiths@sanketika.in>
2022-04-14 17:44:00 +0530

    feat: add test cases

--- api-gateway/src/test/java/org/swasth/apigateway/service/RegistryServiceTest.java
+++ api-gateway/src/test/java/org/swasth/apigateway/service/RegistryServiceTest.java
@@ -3,42 +3,26 @@ package org.swasth.apigateway.service;
 import okhttp3.mockwebserver.MockResponse;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.swasth.apigateway.BaseSpec;
-import org.swasth.apigateway.constants.Constants;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 
 public class RegistryServiceTest extends BaseSpec {
 
     @Test
-    public void check_registry_service_success_scenario() throws Exception {
+    public void check_registry_service_internal_server_exception_scenario() throws Exception {
         server.enqueue(new MockResponse()
-                .setResponseCode(200)
-                .setBody("[{\"test\":\"123\"}]")
+                .setResponseCode(400)
                 .addHeader("Content-Type", "application/json"));
 
         Mockito.when(registryService.getDetails(any(), any())).thenCallRealMethod();
         ReflectionTestUtils.setField(registryService, "registryUrl", "http://localhost:8080");
-        Map<String, Object> result = registryService.getDetails("osid", "1-5e934f90-111d-4f0b-b016-c22d820674e1");
-        assertFalse(result.isEmpty());
-    }
-
-    @Test
-    public void check_registry_service_server_exception_scenario() throws Exception {
-        Mockito.when(registryService.getDetails(any(), any())).thenCallRealMethod();
-        ReflectionTestUtils.setField(registryService, "registryUrl", "http://localhost:8081");
         Exception exception = assertThrows(Exception.class, () -> {
             registryService.getDetails("osid", "1-5e934f90-111d-4f0b-b016-c22d820674e1");
         });
-        assertNotNull(exception);
+        assertEquals("Error in fetching the participant details400", exception.getMessage());
     }
-
 }
