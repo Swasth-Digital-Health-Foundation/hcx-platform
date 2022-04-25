a200749a7435f4d4ee4069fac3c6b672b973a43e
Shiva Rakshith <rakshiths@sanketika.in>
2022-04-25 08:09:23 +0530

    feat: add payload utils

--- hcx-core/hcx-common/src/main/java/org/swasth/common/utils/PayloadUtils.java
+++ hcx-core/hcx-common/src/main/java/org/swasth/common/utils/PayloadUtils.java
@@ -1,6 +1,7 @@
 package org.swasth.common.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.experimental.UtilityClass;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,6 +10,7 @@ import java.util.Map;
 
 import static org.swasth.common.utils.Constants.PAYLOAD;
 
+@UtilityClass
 public class PayloadUtils {
 
     public static String removeEncryptionKey(Map<String, Object> payload) throws JsonProcessingException {
