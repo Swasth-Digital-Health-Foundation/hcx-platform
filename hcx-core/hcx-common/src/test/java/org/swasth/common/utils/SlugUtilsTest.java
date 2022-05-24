d229ec8b215469cec6165593aa29ff518b75485e
Shiva Rakshith <rakshiths@sanketika.in>
2022-05-24 11:01:52 +0530

    feat: add test cases

--- hcx-core/hcx-common/src/test/java/org/swasth/common/utils/SlugUtilsTest.java
+++ hcx-core/hcx-common/src/test/java/org/swasth/common/utils/SlugUtilsTest.java
@@ -7,8 +7,18 @@ public class SlugUtilsTest {
 
     @Test
     public void testMakeSlug() {
-        String sluggified =  SlugUtils.makeSlug("provider apollo hospital");
+        String sluggified =  SlugUtils.makeSlug("-provider--apollo hospital-");
         Assert.assertEquals("provider-apollo-hospital", sluggified);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMakeSlugException() throws Exception {
+        SlugUtils.makeSlug(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testMakeSlugInvalidOutput() throws Exception {
+        SlugUtils.makeSlug("--");
     }
 
 }
