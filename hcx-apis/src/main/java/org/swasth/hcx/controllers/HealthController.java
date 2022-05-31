8b4833780814b2f24a1b35555002f92ddb4f8cb2
Shiva Rakshith <rakshiths@sanketika.in>
2022-05-31 11:09:53 +0530

    Update HealthController.java

--- hcx-apis/src/main/java/org/swasth/hcx/controllers/HealthController.java
+++ hcx-apis/src/main/java/org/swasth/hcx/controllers/HealthController.java
@@ -4,12 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.swasth.common.dto.Response;
-import org.swasth.hcx.managers.HealthCheckManager;
 import org.swasth.common.utils.Constants;
+import org.swasth.hcx.managers.HealthCheckManager;
 
 @RestController
 public class HealthController extends BaseController {
