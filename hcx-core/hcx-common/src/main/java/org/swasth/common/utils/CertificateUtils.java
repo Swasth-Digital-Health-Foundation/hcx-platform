eaa40b8941ff1a2a99c76606eb8f67c4c088e285
Shiva Rakshith <rakshiths@sanketika.in>
2022-08-08 12:47:07 +0530

    feat: add test cases

--- hcx-core/hcx-common/src/main/java/org/swasth/common/utils/CertificateUtils.java
+++ hcx-core/hcx-common/src/main/java/org/swasth/common/utils/CertificateUtils.java
@@ -1,10 +1,13 @@
 package org.swasth.common.utils;
 
+import lombok.experimental.UtilityClass;
+
 import java.io.ByteArrayInputStream;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 
+@UtilityClass
 public class CertificateUtils {
 
     public static Long getExpiry(String key) throws CertificateException {
