f9d7d0013647d688296c1114717d2dd9d88b9dbd
Shiva Rakshith <rakshiths@sanketika.in>
2022-09-22 13:33:52 +0530

    feat: add hcx integrator sdk base implementation

--- integration-sdks/hcx-integrator-sdk/src/main/java/io/hcxprotocol/functions/Incoming.java
+++ integration-sdks/hcx-integrator-sdk/src/main/java/io/hcxprotocol/functions/Incoming.java
@@ -4,7 +4,15 @@ import java.util.Map;
 
 public class Incoming {
 
+    /**
+     * This method is to process the incoming requests.
+     *
+     * @param jwePayload
+     * @param output
+     * @return
+     */
     public boolean processFunction(String jwePayload, Map<String,Object> output) {
         return true;
     }
+
 }
--- integration-sdks/hcx-integrator-sdk/src/main/java/io/hcxprotocol/functions/Outgoing.java
+++ integration-sdks/hcx-integrator-sdk/src/main/java/io/hcxprotocol/functions/Outgoing.java
@@ -4,7 +4,15 @@ import java.util.Map;
 
 public class Outgoing {
 
+    /**
+     * This method is to process the outgoing requests.
+     *
+     * @param jwePayload
+     * @param output
+     * @return
+     */
     public boolean processFunction(String jwePayload, Map<String,Object> output) {
         return true;
     }
+
 }
