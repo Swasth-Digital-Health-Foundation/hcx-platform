ebaec604ffe0097c5368f53967ca39ca14df16d1
Shiva Rakshith <rakshiths@sanketika.in>
2022-09-22 13:21:58 +0530

    feat: add hcx integrator sdk base implementation

---
+++ integration-sdks/hcx-integrator-sdk/src/main/java/io/hcxprotocol/functions/Incoming.java
@@ -0,0 +1,10 @@
+package io.hcxprotocol.functions;
+
+import java.util.Map;
+
+public class Incoming {
+
+    public boolean processFunction(String jwePayload, Map<String,Object> output) {
+        return true;
+    }
+}
---
+++ integration-sdks/hcx-integrator-sdk/src/main/java/io/hcxprotocol/functions/Outgoing.java
@@ -0,0 +1,10 @@
+package io.hcxprotocol.functions;
+
+import java.util.Map;
+
+public class Outgoing {
+
+    public boolean processFunction(String jwePayload, Map<String,Object> output) {
+        return true;
+    }
+}
