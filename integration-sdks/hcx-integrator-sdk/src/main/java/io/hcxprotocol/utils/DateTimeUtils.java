fc7857f5207f40e5d8007751dd632b224805d6ef
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2022-09-29 19:01:23 +0530

    Update DateTimeUtils.java

--- integration-sdks/hcx-integrator-sdk/src/main/java/io/hcxprotocol/utils/DateTimeUtils.java
+++ integration-sdks/hcx-integrator-sdk/src/main/java/io/hcxprotocol/utils/DateTimeUtils.java
@@ -4,11 +4,11 @@ import org.joda.time.DateTime;
 
 public class DateTimeUtils {
 
-    public static boolean validTimestamp(int range, String timestamp) throws Exception {
+    public static boolean validTimestamp(String timestamp) throws Exception {
         try {
             DateTime requestTime = new DateTime(timestamp);
             DateTime currentTime = DateTime.now();
-            return (!requestTime.isBefore(currentTime.minusHours(range)) && !requestTime.isAfter(currentTime));
+            return !requestTime.isAfter(currentTime);
         } catch (Exception e) {
             throw new Exception("Timestamp should be a valid ISO-8061 format, Exception msg: " + e.getMessage());
         }
