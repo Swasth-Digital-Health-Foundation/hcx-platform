fd807d2350a280ffbacee9aa6793e2a0d85a3b48
Shiva Rakshith <rakshiths@sanketika.in>
2022-10-17 12:49:32 +0530

    feat: add validation to hcx integrator configuration variables

--- integration-sdks/hcx-integrator-sdk/src/main/java/io/hcxprotocol/init/HCXIntegrator.java
+++ integration-sdks/hcx-integrator-sdk/src/main/java/io/hcxprotocol/init/HCXIntegrator.java
@@ -2,7 +2,10 @@ package io.hcxprotocol.init;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -20,12 +23,12 @@ public class HCXIntegrator {
     public static HCXIntegrator getInstance() throws Exception {
         if(config == null)
             throw new Exception("Please initialize the configuration variables, in order to initialize the SDK");
+        validateConfig();
         if (hcxIntegrator == null)
             hcxIntegrator = new HCXIntegrator();
         return hcxIntegrator;
     }
-
-
+    
     /**
      * This method is to initialize config factory by passing the configuration as Map.
      *
@@ -43,6 +46,14 @@ public class HCXIntegrator {
      */
     public static void init(String configStr) {
         config = ConfigFactory.parseString(configStr);
+    }
+
+    private static void validateConfig() throws Exception {
+        List<String> props = Arrays.asList("protocolBasePath", "participantCode", "authBasePath", "username", "password", "encryptionPrivateKey", "igUrl");
+        for(String prop: props){
+            if(!config.hasPathOrNull(prop) || StringUtils.isEmpty(config.getString(prop)))
+                throw new Exception(prop + " is missing or has empty value, please add to the configuration.");
+        }
     }
 
     public String getHCXProtocolBasePath() {
