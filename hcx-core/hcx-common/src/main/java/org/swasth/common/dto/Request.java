07861a51b397e9610c1e57904d3a985835577b06
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2022-11-27 11:29:46 +0530

    fix : setstatus updated

--- hcx-core/hcx-common/src/main/java/org/swasth/common/dto/Request.java
+++ hcx-core/hcx-common/src/main/java/org/swasth/common/dto/Request.java
@@ -107,7 +107,7 @@ public class Request {
         return getHeader(STATUS);
     }
 
-    public String setStatus(String status){ setHeaderMap(STATUS, status); }
+    public void  setStatus(String status) { setHeaderMap(STATUS, status);}
     public Map<String, Object> getHcxHeaders() {
         return hcxHeaders;
     }
