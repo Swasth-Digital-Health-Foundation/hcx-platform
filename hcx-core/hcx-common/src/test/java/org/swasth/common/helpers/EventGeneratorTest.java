85f6fafb68b5dbc6fcaf01fb46cfa51c9d7bff5a
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2022-11-28 10:36:05 +0530

    feat : test case updated

--- hcx-core/hcx-common/src/test/java/org/swasth/common/helpers/EventGeneratorTest.java
+++ hcx-core/hcx-common/src/test/java/org/swasth/common/helpers/EventGeneratorTest.java
@@ -6,7 +6,9 @@ import org.swasth.common.dto.Request;
 import org.swasth.common.dto.Response;
 import org.swasth.common.utils.Constants;
 
+import java.awt.*;
 import java.util.*;
+import java.util.List;
 
 import static org.junit.Assert.*;
 import static org.swasth.common.utils.Constants.*;
@@ -76,16 +78,16 @@ public class EventGeneratorTest {
         Map<String,Object> obj = new HashMap<>();
         obj.put("payload","eyJlbmMiOiJBMjU2R0NNIiwKImFsZyI6IlJTQS1PQUVQIiwKIngtaGN4LXNlbmRlcl9jb2RlIjoiIiwKIngtaGN4LXJlY2lwaWVudF9jb2RlIjoiMS0yNzk5YjZhNC1jZjJkLTQ1ZmUtYTVlMS01ZjFjODI5NzllMGQiLAoieC1oY3gtcmVxdWVzdF9pZCI6IjI2YjEwNjBjLTFlODMtNDYwMC05NjEyLWVhMzFlMGNhNTA5MSIsCiJ4LWhjeC1jb3JyZWxhdGlvbl9pZCI6IjVlOTM0ZjkwLTExMWQtNGYwYi1iMDE2LWMyMmQ4MjA2NzRlMSIsCiJ4LWhjeC10aW1lc3RhbXAiOiIyMDIyLTAxLTA2VDA5OjUwOjIzKzAwIiwKIngtaGN4LXN0YXR1cyI6InJlcXVlc3QuaW5pdGlhdGUiLAoieC1oY3gtd29ya2Zsb3dfaWQiOiIxZTgzLTQ2MGEtNGYwYi1iMDE2LWMyMmQ4MjA2NzRlMSIsCiJ4LWhjeC1kZWJ1Z19mbGFnIjoiSW5mbyIsCiJ4LWhjeC1lcnJvcl9kZXRhaWxzIjp7ImVycm9yLmNvZGUiOiAiYmFkLmlucHV0IiwgImVycm9yLm1lc3NhZ2UiOiAiUHJvdmlkZXIgY29kZSBub3QgZm91bmQiLCAidHJhY2UiOiAiIn0sCiJ4LWhjeC1kZWJ1Z19kZXRhaWxzIjp7ImVycm9yLmNvZGUiOiAiYmFkLmlucHV0IiwgImVycm9yLm1lc3NhZ2UiOiAiUHJvdmlkZXIgY29kZSBub3QgZm91bmQiLCJ0cmFjZSI6IiJ9LAoiandzX2hlYWRlciI6eyJ0eXAiOiJKV1QiLCAiYWxnIjoiUlMyNTYifSwKImp3ZV9oZWFkZXIiOnsiYWxnIjoiUlNBLU9BRVAiLCJlbmMiOiJBMjU2R0NNIn0KfQ==.6KB707dM9YTIgHtLvtgWQ8mKwboJW3of9locizkDTHzBC2IlrT1oOQ.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.AxY8DCtDaGlsbGljb3RoZQ.KDlTtXchhZTGufMYmOYGS4HffxPSUrfmqCHXaI9wOGY.Mz-VPPyU4RlcuYv1IwIvzw");
         Map<String,Object> senderDetails = new HashMap<>();
-        senderDetails.put(SENDER_ROLE,List.of("payor"));
+        senderDetails.put(ROLES,List.of("payor"));
         senderDetails.put(PARTICIPANT_NAME,"new-payor-3");
-        senderDetails.put(SENDER_PRIMARY_EMAIL,"newpayor003@gmail.com");
+        senderDetails.put(PRIMARY_EMAIL,"newpayor003@gmail.com");
         Map<String,Object> recipentDetails = new HashMap<>();
-        recipentDetails.put(RECIPIENT_ROLE,List.of("payor"));
+        recipentDetails.put(ROLES,List.of("payor"));
         recipentDetails.put(PARTICIPANT_NAME,"New payor 2");
-        recipentDetails.put(RECIPIENT_PRIMARY_EMAIL,"newpayor002@gmail.com");
-        Request request = new Request(obj, COVERAGE_ELIGIBILITY_CHECK);
+        recipentDetails.put(PRIMARY_EMAIL,"newpayor002@gmail.com");
         obj.put(SENDERDETAILS,senderDetails);
         obj.put(RECIPIENTDETAILS,recipentDetails);
+        Request request = new Request(obj, ACTION);
         request.setApiAction("/test");
         return request;
     }
@@ -295,7 +297,12 @@ public class EventGeneratorTest {
 
     @Test
     public void getSenderDetails() throws Exception {
+        Map<String, Object> output = eventGenerator.generateAuditEvent(getRequest());
         assertEquals("new-payor-3",getRequest().getSenderName());
         assertEquals("New payor 2",getRequest().getRecipientName());
+        assertEquals("newpayor003@gmail.com",getRequest().getSenderPrimaryEmail());
+        assertEquals("newpayor002@gmail.com" ,getRequest().getRecipientPrimaryEmail());
+        assertEquals(List.of("payor"),getRequest().getSenderRole());
+        assertEquals(List.of("payor"),getRequest().getSenderRole());
     }
 }
