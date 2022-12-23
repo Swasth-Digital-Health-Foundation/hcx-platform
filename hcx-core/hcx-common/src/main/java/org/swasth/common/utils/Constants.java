ad6db9a7c150ec9e4514f0308bbb1c05a6840ca8
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2022-12-23 16:36:02 +0530

    Update Constants.java

--- hcx-core/hcx-common/src/main/java/org/swasth/common/utils/Constants.java
+++ hcx-core/hcx-common/src/main/java/org/swasth/common/utils/Constants.java
@@ -61,7 +61,7 @@ public class Constants {
     public static final String PARTICIPANT_DELETE = "/participant/delete";
     public static final String PARTICIPANT_VERIFY = "/participant/verify";
     public static final String PARTICIPANT_GET_INFO = "/participant/get/info/";
-    public static final String PARTICIPANT_SEND_OTP = "/participant/send/otp";
+    public static final String PARTICIPANT_SEND_OTP = "/participant/otp/send";
     public static final String PARTICIPANT_VERIFY_OTP = "/participant/verify/otp";
     // Object Types
     public static final String PARTICIPANT = "participant";
