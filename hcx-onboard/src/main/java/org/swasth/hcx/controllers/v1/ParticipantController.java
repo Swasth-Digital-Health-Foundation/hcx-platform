8b05006ecf0d2664ae7ab2b23bb96f209df9e52c
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2023-01-06 10:51:06 +0530

    Update ParticipantController.java

--- hcx-onboard/src/main/java/org/swasth/hcx/controllers/v1/ParticipantController.java
+++ hcx-onboard/src/main/java/org/swasth/hcx/controllers/v1/ParticipantController.java
@@ -390,6 +390,7 @@ public class ParticipantController extends BaseController {
 
             if(!payorResp.isEmpty()) identityVerification = ACCEPTED;
             updateIdentityVerificationStatus((String) payorResp.getOrDefault(PRIMARY_EMAIL, ""), applicantCode, sponsorCode, identityVerification);
+            System.out.println(payorResp);
             System.out.println(new ParticipantResponse(payorResp));
             return new ResponseEntity<>(new ParticipantResponse(payorResp), HttpStatus.OK);
         } catch (Exception e) {
