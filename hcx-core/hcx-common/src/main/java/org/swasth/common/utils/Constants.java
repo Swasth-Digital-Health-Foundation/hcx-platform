266c6fd46223fc69afeb03fa0e49120ee6336045
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2023-01-17 12:49:37 +0530

    Update Constants.java

--- hcx-core/hcx-common/src/main/java/org/swasth/common/utils/Constants.java
+++ hcx-core/hcx-common/src/main/java/org/swasth/common/utils/Constants.java
@@ -60,7 +60,7 @@ public class Constants {
     public static final String PARTICIPANT_READ = "/participant/read/{participantCode}";
     public static final String PARTICIPANT_UPDATE = "/participant/update";
     public static final String SPONSORS = "sponsors";
-    public static final String VERIFICATIONSTATUS = "verificationstatus";
+    public static final String VERIFICATIONSTATUS = "verificationStatus";
     public static final String PARTICIPANT_DELETE = "/participant/delete";
     public static final String PARTICIPANT_VERIFY = "/participant/verify";
     public static final String PARTICIPANT_GET_INFO = "/participant/getInfo";
