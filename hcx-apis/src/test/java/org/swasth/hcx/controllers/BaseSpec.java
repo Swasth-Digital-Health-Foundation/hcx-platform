406b5aecb364dac1d6d74859be4f3c6583855417
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2023-01-23 19:10:03 +0530

    Update BaseSpec.java

--- hcx-apis/src/test/java/org/swasth/hcx/controllers/BaseSpec.java
+++ hcx-apis/src/test/java/org/swasth/hcx/controllers/BaseSpec.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import org.swasth.AWSClient;
+import org.swasth.ICloudService;
 import org.swasth.auditindexer.function.AuditIndexer;
 import org.swasth.common.dto.Response;
 import org.swasth.common.helpers.EventGenerator;
@@ -87,7 +88,7 @@ public class BaseSpec {
     protected JWTUtils jwtUtils;
 
     @MockBean
-    protected AWSClient awsClient;
+    protected ICloudService awsClient;
 
     @BeforeEach
     public void setup() {
