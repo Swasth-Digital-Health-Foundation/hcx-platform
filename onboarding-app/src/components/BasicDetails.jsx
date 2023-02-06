7835d59aee5cca58361e40088d8434f2dc6e0829
saurabh-singh-17 <saurabhvikashsingh@gmail.com>
2023-02-06 13:44:14 +0530

    Revert "password toggle and error inline done"
    This reverts commit 0f22e4be0ef2df9cc66028f344eba9deb32c2dc1.

--- onboarding-app/src/components/BasicDetails.jsx
+++ onboarding-app/src/components/BasicDetails.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react'
-import { Button, Form, Segment, Grid, Image, Radio, Dimmer, Loader, Icon } from 'semantic-ui-react'
+import { Button, Form, Segment, Grid, Image, Radio, Dimmer, Loader } from 'semantic-ui-react'
 import { post, sendData } from '../service/APIService';
 import { useForm } from "react-hook-form";
 import { ToastContainer, toast, autoClose } from 'react-toastify';
@@ -27,27 +27,16 @@ export const BasicDetails = ({ changeTab, formState, setState }) => {
     const watchPrimaryEmail = watch("primary_email", "")
     const [applicantCode, setApplicantCode] = useState("")
     const [fetchResponse, setFetchResponse] = useState(false)
-    const [formErrors , setFormErrors] = useState({});
 
 
     let query = useQuery();
-    
+
     const getPayor = participantName => {
         const participant = payorList.find(participant => participant.participant_name === participantName);
         if (participant) {
             setPayor(participant)
         }
 
-    }
-
-    const setPrimaryEmailState = email => {
-        setPrimaryEmail(email);
-        setFormErrors({});
-    }
-
-    const setPrimaryMobileState = phone => {
-        setPrimaryMobile(phone);
-        setFormErrors({});
     }
 
     useEffect(() => {
@@ -73,7 +62,6 @@ export const BasicDetails = ({ changeTab, formState, setState }) => {
         setLoader(true)
         setSending(true)
         const jwtToken = query.get("jwt_token");
-        const jwt_token = "Bearer eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJMYU9HdVRrYVpsVEtzaERwUng1R25JaXUwV1A1S3VGUUoyb29WMEZnWGx3In0.eyJleHAiOjE2NzgyNDc4ODEsImlhdCI6MTY3NTY1NTg4MSwianRpIjoiMzQwODdiNDEtMWQyMy00NjlhLWI3NjMtOWVmY2U4Y2FhMzQ4IiwiaXNzIjoiaHR0cDovL2E5ZGQ2M2RlOTFlZTk0ZDU5ODQ3YTEyMjVkYThiMTExLTI3Mzk1NDEzMC5hcC1zb3V0aC0xLmVsYi5hbWF6b25hd3MuY29tOjgwODAvYXV0aC9yZWFsbXMvc3dhc3RoLWhlYWx0aC1jbGFpbS1leGNoYW5nZSIsImF1ZCI6ImFjY291bnQiLCJzdWIiOiJkZmNlMzY3OS02ZDM2LTQ3NzItOGI1NS01NWE1YzQ2MzA2MTAiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJyZWdpc3RyeS1mcm9udGVuZCIsInNlc3Npb25fc3RhdGUiOiI1MGQzZWFlZC1iM2U3LTQ4MGYtYTg4Yy02ZjY4MGZjMWE1ZDMiLCJhY3IiOiIxIiwiYWxsb3dlZC1vcmlnaW5zIjpbImh0dHBzOi8vbG9jYWxob3N0OjQyMDIiLCJodHRwOi8vbG9jYWxob3N0OjQyMDIiLCJodHRwczovL2xvY2FsaG9zdDo0MjAwIiwiaHR0cHM6Ly9uZGVhci54aXYuaW4iLCJodHRwOi8vbG9jYWxob3N0OjQyMDAiLCJodHRwOi8vbmRlYXIueGl2LmluIiwiaHR0cDovLzIwLjE5OC42NC4xMjgiXSwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIkhJRS9ISU8uSENYIiwiZGVmYXVsdC1yb2xlcy1uZGVhciJdfSwicmVzb3VyY2VfYWNjZXNzIjp7ImFjY291bnQiOnsicm9sZXMiOlsibWFuYWdlLWFjY291bnQiLCJtYW5hZ2UtYWNjb3VudC1saW5rcyIsInZpZXctcHJvZmlsZSJdfX0sInNjb3BlIjoicHJvZmlsZSBlbWFpbCIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwicHJlZmVycmVkX3VzZXJuYW1lIjoiaGN4QHN3YXN0aGFwcC5vcmciLCJlbnRpdHkiOlsiT3JnYW5pc2F0aW9uIl0sImVtYWlsIjoiaGN4QHN3YXN0aGFwcC5vcmcifQ.Hv-hvHI_ZbfbX2o5oQXbmWAQkFgzRnuDkHz2MIdUVXola7ImWMdUGIENJxByjJ_zTZChQGJpX1CsHXvJ_vWDkeVRI0wXwSMGl-0_o3A588Lu47XTpXKLuaGlyzarsynCfZAEa6KakuDV-wzepDJDYFimgN5YsShmjPoW8Zw6sRMbZCqz_kM8EYFJRzVBy16YVWB9PXXjZfmbFxqh43O3rgIcPjxjt2I8m_lWlFb4UVFfg9txv4bReq3qlxs75ABD3tKPDrZNedL7yGEWWXelfTw3o9U4lwUrMrrS-5SH9NufMpZ5irRLYCJs1Y-sj0urX2Bgb8lcSosoQZ-j7jPYmQ";
         let formData;
 
         if (isJWTPresent) {
@@ -84,10 +72,8 @@ export const BasicDetails = ({ changeTab, formState, setState }) => {
             formData = [{ "participant": { "participant_name": data.participant_name, "primary_email": data.primary_email, "primary_mobile": data.primary_mobile, "roles": [data.roles] } }];
         }
 
-        console.log("i came in verify");
         post("/participant/verify", JSON.stringify(formData))
             .then((data => {
-                console.log("result of verify", data);
                 toast.success("Form is submitted successfully", {
                     position: toast.POSITION.TOP_CENTER, autoClose: 2000
                 });
@@ -95,18 +81,9 @@ export const BasicDetails = ({ changeTab, formState, setState }) => {
                 changeTab(1)
                 setState({ ...formState, ...(formData[0]), ...{ "participant_code": _.get(data, 'data.result.participant_code'), "identity_verification": _.get(data, 'data.result.identity_verification') } })
             })).catch(err => {
-                // toast.error(_.get(err, 'response.data.error.message') || "Internal Server Error", {
-                //     position: toast.POSITION.TOP_CENTER
-                // });
-                console.log(_.get(err, 'response.data.error.message') );
-                if(_.get(err, 'response.data.error.message') && _.get(err, 'response.data.error.message') == "Username already invited / registered for Organisation"){
-                    setFormErrors({email:_.get(err, 'response.data.error.message'), phone:_.get(err, 'response.data.error.message')});
-                }else{
                 toast.error(_.get(err, 'response.data.error.message') || "Internal Server Error", {
-                     position: toast.POSITION.TOP_CENTER
-                });     
-                }
-                
+                    position: toast.POSITION.TOP_CENTER
+                });
             }).finally(() => {
                 setSending(false)
                 setLoader(false)
@@ -223,14 +200,12 @@ export const BasicDetails = ({ changeTab, formState, setState }) => {
                 {watchRoles === "payor" || fetchResponse === true ? 
                     <Form.Field disabled={sending} className={{ 'error': primaryEmail === '' && 'primary_email' in errors }} required>
                         <label>Email</label>
-                        <input className='input-text' type='email' placeholder='Enter Email' value={primaryEmail} disabled={primaryEmail != '' && ((watchApplicantCode != '' && invalidApplicantCode) || isJWTPresent)} onInput={e => setPrimaryEmailState(e.target.value)} {...register("primary_email", { required: true, pattern: /^\S+@\S+$/i, message: "Email required" })} />
-                        {formErrors.email && (<div style={{"color":"red"}}>This email address already exists</div>)}
+                        <input className='input-text' type='email' placeholder='Enter Email' value={primaryEmail} disabled={primaryEmail != '' && ((watchApplicantCode != '' && invalidApplicantCode) || isJWTPresent)} onInput={e => setPrimaryEmail(e.target.value)} {...register("primary_email", { required: true, pattern: /^\S+@\S+$/i, message: "Email required" })} />
                     </Form.Field> : null}
                 {watchRoles === "payor" || fetchResponse === true ?
                     <Form.Field disabled={sending} className={{ 'error': primaryMobile === '' && 'primary_mobile' in errors }} required>
                         <label>Phone Number</label>
-                        <input className='input-text' placeholder='Enter Phone Number' value={primaryMobile} disabled={primaryMobile != '' && ((watchApplicantCode != '' && invalidApplicantCode) || isJWTPresent)} onInput={e => setPrimaryMobileState(e.target.value)} {...register("primary_mobile", { required: true, pattern: /^[0-9]{10}/i })} />
-                        {formErrors.phone && (<div style={{"color":"red"}}>This phone number already exists</div>)}
+                        <input className='input-text' placeholder='Enter Phone Number' value={primaryMobile} disabled={primaryMobile != '' && ((watchApplicantCode != '' && invalidApplicantCode) || isJWTPresent)} onInput={e => setPrimaryMobile(e.target.value)} {...register("primary_mobile", { required: true, pattern: /^[0-9]{10}/i })} />
                     </Form.Field> : null}
                 {watchRoles === "payor" || fetchResponse === true ?
                     <Form.Field disabled={sending} className={{ 'error': participantName === '' && 'participant_name' in errors }} required>
