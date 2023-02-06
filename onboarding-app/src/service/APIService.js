bdd9d89f8fbcca0aac7e1b534f380e4906a219c0
saurabh-singh-17 <saurabhvikashsingh@gmail.com>
2023-02-06 13:48:15 +0530

    inline errors and password toggle show and hide done

--- onboarding-app/src/service/APIService.js
+++ onboarding-app/src/service/APIService.js
@@ -6,7 +6,6 @@ const baseUrl = process.env.REACT_APP_HCX_PATH;
 const apiVersion = process.env.REACT_APP_HCX_API_VERSION;
 const adminToken = process.env.REACT_APP_HCX_ADMIN_TOKEN;
 
-
 export const post = async (path, body, headers = {}) => {
   return axios({
     method: 'post',
