b39336fd3a8fb987979c1e4c7d806c126a43fcd9
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2023-02-07 18:13:46 +0530

    Update ParticipantController.java

--- hcx-apis/src/main/java/org/swasth/hcx/controllers/v1/ParticipantController.java
+++ hcx-apis/src/main/java/org/swasth/hcx/controllers/v1/ParticipantController.java
@@ -57,7 +57,7 @@ public class ParticipantController extends BaseController {
             participantService.validateCertificates(requestBody);
             return participantService.invite(requestBody, registryUrl, header, participantCode);
         } catch (Exception e) {
-            e.printStackTrace();
+             e.printStackTrace();
             return exceptionHandler(new Response(), e);
         }
     }
