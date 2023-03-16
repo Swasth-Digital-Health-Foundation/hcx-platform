41542abc4b73561c5eb792f524bc4e5b17b1785c
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2023-03-16 12:42:15 +0530

    Update EventGenerator.java

--- hcx-core/hcx-common/src/main/java/org/swasth/common/helpers/EventGenerator.java
+++ hcx-core/hcx-common/src/main/java/org/swasth/common/helpers/EventGenerator.java
@@ -145,37 +145,6 @@ public class EventGenerator {
         return event;
     }
 
-    public Map<String, Object> generateAuditEvent(Request request) {
-        Map<String, Object> event = new HashMap<>();
-        event.put(EID, AUDIT);
-        event.put(HCX_RECIPIENT_CODE, request.getHcxRecipientCode());
-        event.put(HCX_SENDER_CODE, request.getHcxSenderCode());
-        event.put(API_CALL_ID, request.getApiCallId());
-        event.put(CORRELATION_ID, request.getCorrelationId());
-        event.put(WORKFLOW_ID, request.getWorkflowId());
-        event.put(TIMESTAMP, request.getTimestamp());
-        event.put(ERROR_DETAILS, request.getErrorDetails());
-        event.put(DEBUG_DETAILS, request.getDebugDetails());
-        event.put(MID, request.getMid());
-        event.put(ACTION, request.getApiAction());
-        if (StringUtils.isEmpty(request.getStatus()))
-            event.put(STATUS, QUEUED_STATUS);
-        else
-            event.put(STATUS, request.getStatus());
-        event.put(REQUEST_TIME, System.currentTimeMillis());
-        event.put(UPDATED_TIME, System.currentTimeMillis());
-        event.put(ETS, System.currentTimeMillis());
-        event.put(PAYLOAD, request.getPayloadWithoutSensitiveData());
-        event.put(SENDER_ROLE, request.getSenderRole());
-        event.put(RECIPIENT_ROLE, request.getRecipientRole());
-        event.put(SENDER_NAME, request.getSenderName());
-        event.put(RECIPIENT_NAME, request.getRecipientName());
-        event.put(SENDER_PRIMARY_EMAIL, request.getSenderPrimaryEmail());
-        event.put(RECIPIENT_PRIMARY_EMAIL, request.getRecipientPrimaryEmail());
-        getTag(request,event);
-        return event;
-    }
-
     private Map<String, Object> createOnSubscriptionPayload(String subscriptionId, String status) {
         Map<String, Object> event = new HashMap<>();
         event.put(SUBSCRIPTION_ID, subscriptionId);
@@ -212,6 +181,37 @@ public class EventGenerator {
         event.put(HCX_RECIPIENT_CODE, request.getRecipientCode());
         event.put(ETS, System.currentTimeMillis());
         event.put(STATUS, status);
+        return event;
+    }
+
+    public Map<String, Object> generateAuditEvent(Request request) {
+        Map<String, Object> event = new HashMap<>();
+        event.put(EID, AUDIT);
+        event.put(HCX_RECIPIENT_CODE, request.getHcxRecipientCode());
+        event.put(HCX_SENDER_CODE, request.getHcxSenderCode());
+        event.put(API_CALL_ID, request.getApiCallId());
+        event.put(CORRELATION_ID, request.getCorrelationId());
+        event.put(WORKFLOW_ID, request.getWorkflowId());
+        event.put(TIMESTAMP, request.getTimestamp());
+        event.put(ERROR_DETAILS, request.getErrorDetails());
+        event.put(DEBUG_DETAILS, request.getDebugDetails());
+        event.put(MID, request.getMid());
+        event.put(ACTION, request.getApiAction());
+        if (StringUtils.isEmpty(request.getStatus()))
+            event.put(STATUS, QUEUED_STATUS);
+        else
+            event.put(STATUS, request.getStatus());
+        event.put(REQUEST_TIME, System.currentTimeMillis());
+        event.put(UPDATED_TIME, System.currentTimeMillis());
+        event.put(ETS, System.currentTimeMillis());
+        event.put(PAYLOAD, request.getPayloadWithoutSensitiveData());
+        event.put(SENDER_ROLE, request.getSenderRole());
+        event.put(RECIPIENT_ROLE, request.getRecipientRole());
+        event.put(SENDER_NAME, request.getSenderName());
+        event.put(RECIPIENT_NAME, request.getRecipientName());
+        event.put(SENDER_PRIMARY_EMAIL, request.getSenderPrimaryEmail());
+        event.put(RECIPIENT_PRIMARY_EMAIL, request.getRecipientPrimaryEmail());
+        getTag(request,event);
         return event;
     }
 
