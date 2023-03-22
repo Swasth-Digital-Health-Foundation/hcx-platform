bf1f99aa06ac73b5921a40f6522bf18aad4aac72
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2023-03-22 22:56:17 +0530

    feat: add retry api

--- hcx-core/hcx-common/src/main/java/org/swasth/common/utils/Constants.java
+++ hcx-core/hcx-common/src/main/java/org/swasth/common/utils/Constants.java
@@ -54,6 +54,9 @@ public class Constants {
     public static final String AUDIT_SEARCH = "/audit/search";
     public static final String AUDIT_NOTIFICATION_SEARCH = "/audit/notification/search";
 
+    // Supportability APIs
+    public static final String REQUEST_RETRY = "/request/retry/{mid}";
+
     // Registry Participant APIs
     public static final String PARTICIPANT_CREATE = "/participant/create";
     public static final String PARTICIPANT_SEARCH = "/participant/search";
@@ -91,6 +94,7 @@ public class Constants {
 
     //event generator props
     public static final String MID = "mid";
+    public static final String DATA = "data";
     public static final String PAYLOAD = "payload";
     public static final String ETS = "ets";
     public static final String ACTION = "action";
