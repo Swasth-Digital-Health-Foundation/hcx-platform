a1d5343a8b9a225b450db9819d4735b2b4138926
Shiva Rakshith <80666319+shiva-rakshith@users.noreply.github.com>
2023-03-27 10:34:51 +0530

    feat: add onboard filter

---
+++ api-gateway/src/main/java/org/swasth/apigateway/filters/OnboardFilter.java
@@ -0,0 +1,45 @@
+package org.swasth.apigateway.filters;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.stereotype.Component;
+import org.swasth.apigateway.handlers.RequestHandler;
+import org.swasth.common.utils.JSONUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.CACHED_REQUEST_BODY_ATTR;
+import static org.swasth.common.utils.Constants.JWT_TOKEN;
+
+@Component
+public class OnboardFilter extends AbstractGatewayFilterFactory<OnboardFilter.Config> {
+    @Autowired
+    RequestHandler requestHandler;
+
+    public OnboardFilter() {
+        super(OnboardFilter.Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        Map<String, Object> requestBody = new HashMap<>();
+        return ((exchange, chain) -> {
+            try {
+                String token = Objects.requireNonNull(exchange.getRequest().getHeaders().get("Authorization").toString().substring(7));
+                requestBody.putAll(JSONUtils.deserialize(String.valueOf(StandardCharsets.UTF_8.decode(((DataBuffer) exchange.getAttribute(CACHED_REQUEST_BODY_ATTR)).asByteBuffer())), HashMap.class));
+                requestBody.put(JWT_TOKEN, token);
+            } catch (Exception e) {
+                throw new RuntimeException();
+            }
+            return requestHandler.getUpdatedBody(exchange, chain, requestBody);
+        });
+    }
+
+    public static class Config {
+    }
+}
