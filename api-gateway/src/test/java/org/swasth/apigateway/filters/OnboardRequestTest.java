a8d96f90fd6cc9ce2d3b8ab041e151212b01f558
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2023-03-31 17:39:34 +0530

    feat : test cases for onboard filter

--- api-gateway/src/test/java/org/swasth/apigateway/filters/OnboardRequestTest.java
+++ api-gateway/src/test/java/org/swasth/apigateway/filters/OnboardRequestTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.swasth.apigateway.BaseSpec;
 import org.swasth.common.utils.Constants;
+
 import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -31,11 +32,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
         client.post().uri(versionPrefix + Constants.PARTICIPANT_ONBOARD_UPDATE)
                  .header("X-jwt-sub", "f7c0e759-bec3-431b-8c4f-6b294d103a74")
-                 .header(Constants.AUTHORIZATION, "Bearer " )
-                 .bodyValue(getRequestBody())
+                 .header(Constants.AUTHORIZATION, getUpdateUserToken())
+                 .bodyValue("")
                  .exchange()
                  .expectBody(Map.class)
-                 .consumeWith(result -> assertEquals(HttpStatus.UNAUTHORIZED, result.getStatus()));
+                 .consumeWith(result -> assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result.getStatus()));
      }
 }
 
