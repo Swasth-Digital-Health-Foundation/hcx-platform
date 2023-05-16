67b305477cf16006300f758e0729eaccb5ad4712
shiva-rakshith <rakshiths@sanketika.in>
2023-05-16 23:35:42 +0530

    feat: add webclient

--- api-gateway/src/main/java/org/swasth/apigateway/config/WebClientConfiguration.java
+++ api-gateway/src/main/java/org/swasth/apigateway/config/WebClientConfiguration.java
@@ -1,42 +1,33 @@
 package org.swasth.apigateway.config;
 
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.web.codec.CodecCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.codec.ClientCodecConfigurer;
+import org.springframework.http.client.reactive.ClientHttpConnector;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.ExchangeStrategies;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfiguration {
 
-    private final CodecCustomizer codecCustomizer;
+    @Bean
+    public WebClient webClient(CodecCustomizer codecCustomizer) {
+        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
+                .codecs(configurer -> codecCustomizer.customize(configurer))
+                .build();
 
-    public WebClientConfiguration(ObjectProvider<CodecCustomizer> codecCustomizerProvider) {
-        this.codecCustomizer = codecCustomizerProvider.getIfAvailable();
+        ClientHttpConnector httpConnector = new ReactorClientHttpConnector();
+
+        return WebClient.builder()
+                .clientConnector(httpConnector)
+                .exchangeStrategies(exchangeStrategies)
+                .build();
     }
 
     @Bean
-    public WebClient webClient() {
-        WebClient.Builder webClientBuilder = WebClient.builder();
-
-        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
-                .codecs(this::customizeCodecs)
-                .build();
-
-        webClientBuilder.exchangeStrategies(exchangeStrategies);
-
-        return webClientBuilder.build();
+    public CodecCustomizer codecCustomizer() {
+        return configurer -> configurer.defaultCodecs().maxInMemorySize(16777216); // Set the maximum buffer size to 524,288 bytes (512 KB)
     }
 
-    private void customizeCodecs(ClientCodecConfigurer configurer) {
-        if (codecCustomizer != null) {
-            this.codecCustomizer.customize(configurer);
-        }
-
-        configurer.defaultCodecs()
-                .maxInMemorySize(16777216); // Set the maximum buffer size to 524,288 bytes (512 KB)
-    }
 }
-
