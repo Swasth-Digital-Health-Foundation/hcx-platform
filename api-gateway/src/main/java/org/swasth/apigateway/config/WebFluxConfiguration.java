03a3e002ce9fba0123a20a92431445743bfb2924
shiva-rakshith <rakshiths@sanketika.in>
2023-05-16 19:18:22 +0530

    feat: update request memory limit

---
+++ api-gateway/src/main/java/org/swasth/apigateway/config/WebFluxConfiguration.java
@@ -0,0 +1,13 @@
+package org.swasth.apigateway.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class WebFluxConfiguration implements WebFluxConfigurer {
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        configurer.defaultCodecs().maxInMemorySize(10 * 1024 * 1024);
+    }
+}
