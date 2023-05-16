83095edb2096741a84eb38bf4e7ffea38f0972d3
shiva-rakshith <rakshiths@sanketika.in>
2023-05-16 20:02:26 +0530

    feat: update variable

--- api-gateway/src/main/java/org/swasth/apigateway/config/WebClientConfiguration.java
+++ api-gateway/src/main/java/org/swasth/apigateway/config/WebClientConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.http.codec.ClientCodecConfigurer;
 import org.springframework.web.reactive.function.client.WebClient;
 
