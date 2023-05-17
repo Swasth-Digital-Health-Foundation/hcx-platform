1f1beff163e5048100b29a32643e2bebe6965360
shiva-rakshith <rakshiths@sanketika.in>
2023-05-17 16:52:24 +0530

    feat: update request handler

--- api-gateway/src/main/java/org/swasth/apigateway/handlers/RequestHandler.java
+++ api-gateway/src/main/java/org/swasth/apigateway/handlers/RequestHandler.java
@@ -1,6 +1,5 @@
 package org.swasth.apigateway.handlers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
@@ -14,14 +13,10 @@ import java.util.Map;
 public class RequestHandler {
 
     public Mono<Void> getUpdatedBody(ServerWebExchange exchange, GatewayFilterChain chain, Map<String,Object> request) throws Exception {
-        try {
-            byte[] modifiedBody = JSONUtils.serialize(request).getBytes();
-            RequestDecorator requestDecorator = new RequestDecorator(exchange.getRequest(), modifiedBody);
-            exchange = exchange.mutate().request(requestDecorator.mutate().header("Content-Length", String.valueOf(modifiedBody.length)).build()).build();
-            return chain.filter(exchange);
-        } catch (JsonProcessingException e) {
-            throw new Exception("Error while updating the request body");
-        }
-     }
+        byte[] modifiedBody = JSONUtils.serialize(request).getBytes();
+        RequestDecorator requestDecorator = new RequestDecorator(exchange.getRequest(), modifiedBody);
+        exchange = exchange.mutate().request(requestDecorator.mutate().header("Content-Length", String.valueOf(modifiedBody.length)).build()).build();
+        return chain.filter(exchange);
+    }
 
 }
