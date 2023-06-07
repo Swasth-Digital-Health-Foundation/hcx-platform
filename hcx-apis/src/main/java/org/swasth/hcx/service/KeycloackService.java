6ef751200229ed7411500a6e1435237486b94e28
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2023-06-07 18:22:37 +0530

    feat : keyclaock API

---
+++ hcx-apis/src/main/java/org/swasth/hcx/service/KeycloackService.java
@@ -0,0 +1,16 @@
+package org.swasth.hcx.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class KeycloackService {
+
+
+    public void generateToken(){
+
+
+    }
+
+
+
+}
