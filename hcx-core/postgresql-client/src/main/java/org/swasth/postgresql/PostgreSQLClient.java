a88393b56822b6964ffd84d9b19533b30a260cdc
shiva-rakshith <rakshiths@sanketika.in>
2023-07-07 10:49:37 +0530

    feat: enhance postgres client

--- hcx-core/postgresql-client/src/main/java/org/swasth/postgresql/PostgreSQLClient.java
+++ hcx-core/postgresql-client/src/main/java/org/swasth/postgresql/PostgreSQLClient.java
@@ -9,60 +9,88 @@ public class PostgreSQLClient implements IDatabaseService {
     private final String url;
     private final String user;
     private final String password;
-    private final Connection connection;
-    private final Statement statement;
+    private Connection connection;
+    private Statement statement;
 
     public PostgreSQLClient(String url, String user, String password) throws ClientException, SQLException {
         this.url = url;
         this.user = user;
         this.password = password;
-        this.connection = getConnection();
-        this.statement = this.connection.createStatement();
+        initializeConnection();
+    }
+
+    private void initializeConnection() throws ClientException {
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.createStatement();
+        } catch (SQLException e) {
+            throw new ClientException("Error connecting to the PostgreSQL server: " + e.getMessage());
+        }
     }
 
     public Connection getConnection() throws ClientException {
-        Connection conn;
         try {
-            conn = DriverManager.getConnection(url, user, password);
-        } catch (Exception e) {
+            if (connection == null || connection.isClosed()) {
+                initializeConnection();
+            }
+        } catch (SQLException e) {
             throw new ClientException("Error connecting to the PostgreSQL server: " + e.getMessage());
         }
-        return conn;
+        return connection;
     }
 
     public void close() throws SQLException {
-        statement.close();
-        connection.close();
+        if (statement != null) {
+            statement.close();
+        }
+        if (connection != null) {
+            connection.close();
+        }
     }
 
     public boolean execute(String query) throws ClientException {
         try {
-            return statement.execute(query);
-        } catch (Exception e) {
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            boolean result = stmt.execute(query);
+            stmt.close();
+            return result;
+        } catch (SQLException e) {
             throw new ClientException("Error while performing database operation: " + e.getMessage());
         }
     }
 
     public ResultSet executeQuery(String query) throws ClientException {
         try {
-            return statement.executeQuery(query);
-        } catch (Exception e) {
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet resultSet = stmt.executeQuery(query);
+            stmt.close();
+            return resultSet;
+        } catch (SQLException e) {
             throw new ClientException("Error while performing database operation: " + e.getMessage());
         }
     }
 
     public void addBatch(String query) throws ClientException {
         try {
-             statement.addBatch(query);
-        } catch (Exception e) {
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            stmt.addBatch(query);
+            stmt.close();
+        } catch (SQLException e) {
             throw new ClientException("Error while performing database operation: " + e.getMessage());
         }
     }
 
     public int[] executeBatch() throws ClientException {
         try {
-            return statement.executeBatch();
-        } catch (Exception e) {
+            Connection conn = getConnection();
+            Statement stmt = conn.createStatement();
+            int[] result = stmt.executeBatch();
+            stmt.close();
+            return result;
+        } catch (SQLException e) {
             throw new ClientException("Error while performing database operation: " + e.getMessage());
         }
     }
