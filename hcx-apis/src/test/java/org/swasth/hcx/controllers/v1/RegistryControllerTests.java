695cd05baa4c41e7424dc2369a8f07f408de4167
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2023-08-06 15:46:37 +0530

    Update RegistryControllerTests.java

--- hcx-apis/src/test/java/org/swasth/hcx/controllers/v1/RegistryControllerTests.java
+++ hcx-apis/src/test/java/org/swasth/hcx/controllers/v1/RegistryControllerTests.java
@@ -1,19 +1,19 @@
-package org.swasth.hcx.controllers.v1;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
-import org.springframework.test.web.servlet.MvcResult;
-import org.swasth.common.utils.Constants;
-import org.swasth.hcx.controllers.BaseSpec;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
-class RegistryControllerTests extends BaseSpec {
-
-
+//package org.swasth.hcx.controllers.v1;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.MediaType;
+//import org.springframework.mock.web.MockHttpServletResponse;
+//import org.springframework.test.web.servlet.MvcResult;
+//import org.swasth.common.utils.Constants;
+//import org.swasth.hcx.controllers.BaseSpec;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+//
+//class RegistryControllerTests extends BaseSpec {
+//
+//
 //    @Test
 //    void participant_remove_user_interrupt_exception() throws Exception {
 //        Thread.currentThread().interrupt();
@@ -39,4 +39,4 @@ class RegistryControllerTests extends BaseSpec {
 //        int status = response.getStatus();
 //        assertEquals(500, status);
 //    }
-}
+//}
