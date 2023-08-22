7815ac47087130154624b84e4285ea932bd76dd5
Abhishek gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2023-08-22 12:36:22 +0530

    Update RegistryControllerTests.java

--- hcx-apis/src/test/java/org/swasth/hcx/controllers/v1/RegistryControllerTests.java
+++ hcx-apis/src/test/java/org/swasth/hcx/controllers/v1/RegistryControllerTests.java
@@ -1,42 +1,42 @@
-//package org.swasth.hcx.controllers.v1;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.http.HttpHeaders;
-//import org.springframework.http.MediaType;
-//import org.springframework.mock.web.MockHttpServletResponse;
-//import org.springframework.test.web.servlet.MvcResult;
-//import org.swasth.common.utils.Constants;
-//import org.swasth.hcx.controllers.BaseSpec;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-//
-//class RegistryControllerTests extends BaseSpec {
-//
-//
-//    @Test
-//    void participant_remove_user_interrupt_exception() throws Exception {
-//        Thread.currentThread().interrupt();
-//        MvcResult mvcResult = mockMvc.perform(post(Constants.VERSION_PREFIX + Constants.PARTICIPANT_USER_REMOVE)
-//                        .content(getParticipantAddBody())
-//                        .header(HttpHeaders.AUTHORIZATION, getAddUserToken())
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andReturn();
-//        MockHttpServletResponse response = mvcResult.getResponse();
-//        int status = response.getStatus();
-//        assertEquals(500, status);
-//    }
-//
-//    @Test
-//    void participant_add_user_interrupt_exception() throws Exception {
-//        Thread.currentThread().interrupt();
-//        MvcResult mvcResult = mockMvc.perform(post(Constants.VERSION_PREFIX + Constants.PARTICIPANT_USER_ADD)
-//                        .content(getParticipantAddBody())
-//                        .header(HttpHeaders.AUTHORIZATION, getAddUserToken())
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andReturn();
-//        MockHttpServletResponse response = mvcResult.getResponse();
-//        int status = response.getStatus();
-//        assertEquals(500, status);
-//    }
-//}
+package org.swasth.hcx.controllers.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MvcResult;
+import org.swasth.common.utils.Constants;
+import org.swasth.hcx.controllers.BaseSpec;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+class RegistryControllerTests extends BaseSpec {
+
+
+    @Test
+    void participant_remove_user_interrupt_exception() throws Exception {
+        Thread.currentThread().interrupt();
+        MvcResult mvcResult = mockMvc.perform(post(Constants.VERSION_PREFIX + Constants.PARTICIPANT_USER_REMOVE)
+                        .content(getParticipantAddBody())
+                        .header(HttpHeaders.AUTHORIZATION, getAddUserToken())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        int status = response.getStatus();
+        assertEquals(500, status);
+    }
+
+    @Test
+    void participant_add_user_interrupt_exception() throws Exception {
+        Thread.currentThread().interrupt();
+        MvcResult mvcResult = mockMvc.perform(post(Constants.VERSION_PREFIX + Constants.PARTICIPANT_USER_ADD)
+                        .content(getParticipantAddBody())
+                        .header(HttpHeaders.AUTHORIZATION, getAddUserToken())
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        int status = response.getStatus();
+        assertEquals(500, status);
+    }
+}
