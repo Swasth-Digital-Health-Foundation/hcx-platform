fc615f33c80eee1bf56c74196d1160d36636f0e2
yashashk <yashashk@sanketika.in>
2023-09-13 12:19:09 +0530

    Issue #567 feat: added loading button and toast component.

--- benificiary-app/src/main.tsx
+++ benificiary-app/src/main.tsx
@@ -5,9 +5,12 @@ import App from './App';
 import './index.css';
 import './satoshi.css';
 import './simple-datatables.css';
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ToastContainer></ToastContainer>
     <Router>
       <App />
     </Router>
