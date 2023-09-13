3ac9c7220a3a197a6ddaaa82256e019eb5f638b6
yashashk <yashashk@sanketika.in>
2023-09-13 12:17:06 +0530

    Issue #567 refactor: changed table to cards.

---
+++ benificiary-app/src/components/ActiveClaimCycleCard.tsx
@@ -0,0 +1,48 @@
+const ActiveClaimCycleCard = () => {
+  return (
+    <div>
+      <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="flex flex-col p-4">
+          <h2 className="block text-left font-medium text-black dark:text-white">
+            Date :
+          </h2>
+          <h2 className="block text-left font-medium text-black dark:text-white">
+            Insurance ID :
+          </h2>
+          <h2 className="block text-left font-medium text-black dark:text-white">
+            Request Type :
+          </h2>
+          <h2 className="block text-left font-medium text-black dark:text-white">
+            Request ID :
+          </h2>
+          <h2 className="block text-left font-medium text-black dark:text-white">
+            Status :
+          </h2>
+          <span className="cursor-pointer text-right underline">View</span>
+        </div>
+      </div>
+      <div className="mt-2 rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+        <div className="flex flex-col p-4">
+          <h2 className="block text-left font-medium text-black dark:text-white">
+            Date :
+          </h2>
+          <h2 className="block text-left font-medium text-black dark:text-white">
+            Insurance ID :
+          </h2>
+          <h2 className="block text-left font-medium text-black dark:text-white">
+            Request Type :
+          </h2>
+          <h2 className="block text-left font-medium text-black dark:text-white">
+            Request ID :
+          </h2>
+          <h2 className="block text-left font-medium text-black dark:text-white">
+            Status :
+          </h2>
+          <span className="cursor-pointer text-right underline">View</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ActiveClaimCycleCard;
