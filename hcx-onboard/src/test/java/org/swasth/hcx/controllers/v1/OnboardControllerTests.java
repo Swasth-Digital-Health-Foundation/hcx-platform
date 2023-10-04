d128cb227926a0753f369aa68417a0fec94f47b6
KrutikaPhirangi <krutikap@sanketika.in>
2023-10-04 14:38:22 +0530

    Tests

--- hcx-onboard/src/test/java/org/swasth/hcx/controllers/v1/OnboardControllerTests.java
+++ hcx-onboard/src/test/java/org/swasth/hcx/controllers/v1/OnboardControllerTests.java
@@ -1,26 +1,51 @@
 package org.swasth.hcx.controllers.v1;
 
-import okhttp3.mockwebserver.MockWebServer;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MvcResult;
+import org.swasth.common.utils.Constants;
+import org.swasth.common.utils.JSONUtils;
 import org.swasth.hcx.controllers.BaseSpec;
+import org.swasth.hcx.services.OnboardService;
 
-import java.io.IOException;
-import java.net.InetAddress;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 class OnboardControllerTests extends BaseSpec{
+    @MockBean
+    private OnboardService onboardService;
 
-    private final MockWebServer registryServer =  new MockWebServer();
-
-    @BeforeEach
-    public void start() throws IOException {
-        registryServer.start(InetAddress.getByName("localhost"),8082);
+    @Test
+    public void test_sendVerification_link_success() throws Exception {
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("participant_code", "payr_testpa_313025@swasth-hcx");
+        requestBody.put("channel", List.of("EMAIL","MOBILE"));
+        String requestBodyJson = JSONUtils.serialize(requestBody);
+        ResponseEntity<Object> expectedResponseEntity = new ResponseEntity<>(requestBodyJson, HttpStatus.OK);;
+        when(onboardService.sendVerificationLink(requestBody)).thenReturn(expectedResponseEntity);
+        MvcResult mvcResult = mockMvc.perform(post(Constants.VERSION_PREFIX + Constants.PARTICIPANT_VERIFICATION_LINK_SEND).content(requestBodyJson).header(HttpHeaders.AUTHORIZATION,getAuthorizationHeader()).contentType(MediaType.APPLICATION_JSON)).andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        int status = response.getStatus();
+        Assertions.assertEquals(200, status);
     }
 
-    @AfterEach
-    public void teardown() throws IOException, InterruptedException {
-        registryServer.shutdown();
-        Thread.sleep(2000);
+    @Test
+    public void test_sendVerification_link_exception() throws Exception {
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("channel", List.of("EMAIL","MOBILE"));
+        MvcResult mvcResult = mockMvc.perform(post(Constants.VERSION_PREFIX + Constants.PARTICIPANT_VERIFICATION_LINK_SEND).content(requestBody.toString()).header(HttpHeaders.AUTHORIZATION,getAuthorizationHeader()).contentType(MediaType.APPLICATION_JSON)).andReturn();
+        MockHttpServletResponse response = mvcResult.getResponse();
+        int status = response.getStatus();
+        Assertions.assertEquals(400, status);
     }
-
 }
