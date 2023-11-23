8e90ca643dd9b0592ba9fba831b2ab932959582c
yashashk <yashashk@sanketika.in>
2023-11-23 10:32:01 +0530

    Issue #575 refactor: camera opening issue resolved.

--- opd-app/src/components/ActiveClaimCycleCard.tsx
+++ opd-app/src/components/ActiveClaimCycleCard.tsx
@@ -83,8 +83,8 @@ const ActiveClaimCycleCard = (Props: any) => {
           >
             <div className="flex items-center justify-end gap-2">
               <p>View details</p>
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
               </svg>
             </div>
           </span>
--- opd-app/src/components/Html5QrcodeScannerPlugin/Html5QrcodeScannerPlugin.tsx
+++ opd-app/src/components/Html5QrcodeScannerPlugin/Html5QrcodeScannerPlugin.tsx
@@ -1,133 +1,71 @@
-// import { Html5QrcodeScanner } from "html5-qrcode";
-// import { useState } from "react";
-
-// const qrcodeRegionId = "html5qr-code-full-region";
-
-// // Creates the configuration object for Html5QrcodeScanner.
-// const createConfig = (props: any) => {
-//   let config: any = {};
-//   if (props.fps) {
-//     config.fps = props.fps;
-//   }
-//   if (props.qrbox) {
-//     config.qrbox = props.qrbox;
-//   }
-//   if (props.aspectRatio) {
-//     config.aspectRatio = props.aspectRatio;
-//   }
-//   if (props.disableFlip !== undefined) {
-//     config.disableFlip = props.disableFlip;
-//   }
-//   return config;
-// };
-
-// const Html5QrcodePlugin = (props: any) => {
-//   const [initialized, setInitialized] = useState(props.setInitialized);
-//   const [html5QrcodeScanner, setHtml5QrcodeScanner] = useState<any>(null);
-//   const [scan, setScan] = useState(false)
-
-//   const initializeScanner = async () => {
-//     try {
-//       const config = createConfig(props);
-//       const verbose = props.verbose === true;
-//       const stream = await navigator.mediaDevices.getUserMedia({
-//         video: {
-//           facingMode: 'environment',
-//         },
-//       });
-//       const scanner = new Html5QrcodeScanner(qrcodeRegionId, config, verbose);
-//       scanner.render(props.qrCodeSuccessCallback, props.qrCodeErrorCallback);
-//       setHtml5QrcodeScanner(scanner);
-//       setInitialized(true);
-//     } catch (error) {
-//       console.error("Failed to initialize the QR code scanner: ", error);
-//     }
-//   };
-
-//   const startScanner = () => {
-//     if (initialized) {
-//       initializeScanner();
-//       setScan(!scan)
-//     }
-//   };
-
-//   const stopScanner = () => {
-//     if (html5QrcodeScanner) {
-//       html5QrcodeScanner.clear().catch((error: any) => {
-//         console.error("Failed to clear html5QrcodeScanner. ", error);
-//       });
-//       setScan(!scan)
-//     }
-//   };
-
-//   return (
-//     <>
-//       <div className="flex justify-center gap-6 text-center">
-//         <button>Scan provider QR code to capture patient details</button>
-//         {/* <CustomButton disabled={false} onClick={startScanner} text={"Scan"} className="py-2 px-1 w-20"/> */}
-//       </div>
-//       <button
-//         disabled={false}
-//         onClick={!scan ? startScanner : stopScanner}
-//         className={`align-center m-auto mb-2 mt-1 flex w-auto px-5 h-10 justify-center rounded pt-1.5 font-medium text-gray
-//             cursor-pointer bg-primary text-white`}
-//       >
-//         {!scan ? "scan" : "stop scanning"}
-//       </button>
-//       <div id={qrcodeRegionId} />
-//     </>
-//   );
-// };
-
-// export default Html5QrcodePlugin;
-
 import { Html5QrcodeScanner } from 'html5-qrcode';
 import { useEffect } from 'react';
 
 const qrcodeRegionId = "html5qr-code-full-region";
 
-// Creates the configuration object for Html5QrcodeScanner.
-const createConfig = (props:any) => {
-    let config:any = {};
-    if (props.fps) {
-        config.fps = props.fps;
-    }
-    if (props.qrbox) {
-        config.qrbox = props.qrbox;
-    }
-    if (props.aspectRatio) {
-        config.aspectRatio = props.aspectRatio;
-    }
-    if (props.disableFlip !== undefined) {
-        config.disableFlip = props.disableFlip;
-    }
-    return config;
+const createConfig = (props: any) => {
+  let config: any = {};
+  if (props.fps) {
+    config.fps = props.fps;
+  }
+  if (props.qrbox) {
+    config.qrbox = props.qrbox;
+  }
+  if (props.aspectRatio) {
+    config.aspectRatio = props.aspectRatio;
+  }
+  if (props.disableFlip !== undefined) {
+    config.disableFlip = props.disableFlip;
+  }
+  return config;
 };
 
-const Html5QrcodePlugin = (props:any) => {
+const Html5QrcodePlugin = (props: any) => {
+  useEffect(() => {
+    const config = createConfig(props);
+    const verbose = props.verbose === true;
 
-    useEffect(() => {
-        // when component mounts
-        const config = createConfig(props);
-        const verbose = props.verbose === true;
-        // Suceess callback is required.
+    // Request camera permission
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then(() => {
+        // Permission granted, proceed with scanner setup
         if (!(props.qrCodeSuccessCallback)) {
-            throw "qrCodeSuccessCallback is required callback.";
+          throw "qrCodeSuccessCallback is a required callback.";
         }
-        const html5QrcodeScanner = new Html5QrcodeScanner(qrcodeRegionId, config, verbose);
-        html5QrcodeScanner.render(props.qrCodeSuccessCallback, props.qrCodeErrorCallback);
 
-        // cleanup function when component will unmount
+        const html5QrcodeScanner = new Html5QrcodeScanner(
+          qrcodeRegionId,
+          config,
+          verbose
+        );
+
+        html5QrcodeScanner.render(
+          props.qrCodeSuccessCallback,
+          props.qrCodeErrorCallback
+        );
+
+        // Cleanup function when component will unmount
         return () => {
-            html5QrcodeScanner.clear().catch(error => {
-                console.error("Failed to clear html5QrcodeScanner. ", error);
-            });
+          html5QrcodeScanner.clear().catch((error) => {
+            console.error("Failed to clear html5QrcodeScanner. ", error);
+          });
         };
-    }, []);
+      })
+      .catch((error) => {
+        // Handle permission denied or other errors
+        console.error("Failed to access camera. ", error);
+      });
+  }, []);
 
-    return (
-        <div id={qrcodeRegionId} />
-    );
+  return (
+    <>
+      <div className='text-center'>
+        Scan provider QR code to capture patient details
+      </div>
+      <div id={qrcodeRegionId} />
+    </>
+  );
 };
 
 export default Html5QrcodePlugin;
