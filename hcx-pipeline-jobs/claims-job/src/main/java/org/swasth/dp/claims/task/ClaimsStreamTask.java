526da577041814ce24eb78e42d3596b7922f6792
KrutikaPhirangi <138781661+KrutikaPhirangi@users.noreply.github.com>
2024-01-03 17:16:56 +0530

    flink version update

--- hcx-pipeline-jobs/claims-job/src/main/java/org/swasth/dp/claims/task/ClaimsStreamTask.java
+++ hcx-pipeline-jobs/claims-job/src/main/java/org/swasth/dp/claims/task/ClaimsStreamTask.java
@@ -3,11 +3,12 @@ package org.swasth.dp.claims.task;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.swasth.dp.claims.functions.ClaimsProcessFunction;
 import org.swasth.dp.core.function.ContextEnrichmentFunction;
 import org.swasth.dp.core.job.BaseJobConfig;
@@ -46,9 +47,9 @@ public class ClaimsStreamTask {
 
   void process(BaseJobConfig baseJobConfig) throws Exception {
     StreamExecutionEnvironment env = FlinkUtil.getExecutionContext(baseJobConfig);
-    SourceFunction<Map<String,Object>> kafkaConsumer = kafkaConnector.kafkaMapSource(config.kafkaInputTopic);
+    KafkaSource kafkaConsumer =  kafkaConnector.kafkaMapSource(config.kafkaInputTopic);
 
-    SingleOutputStreamOperator<Map<String,Object>> enrichedStream = env.addSource(kafkaConsumer, config.claimsConsumer)
+    SingleOutputStreamOperator enrichedStream = env.fromSource(kafkaConsumer, WatermarkStrategy.noWatermarks(), config.claimsConsumer)
             .uid(config.claimsConsumer).setParallelism(config.consumerParallelism)
             .rebalance()
             .process(new ContextEnrichmentFunction(config, TypeExtractor.getForClass(String.class))).setParallelism(config.downstreamOperatorsParallelism);
@@ -57,7 +58,7 @@ public class ClaimsStreamTask {
             .process(new ClaimsProcessFunction(config)).setParallelism(config.downstreamOperatorsParallelism);
 
     /** Sink for audit events */
-    eventStream.getSideOutput(config.auditOutputTag()).addSink(kafkaConnector.kafkaStringSink(config.auditTopic()))
+    eventStream.getSideOutput(config.auditOutputTag()).sinkTo(kafkaConnector.kafkaStringSink(config.auditTopic()))
             .name(config.auditProducer()).uid(config.auditProducer()).setParallelism(config.downstreamOperatorsParallelism);
 
     System.out.println(config.jobName() + " is processing");
--- hcx-pipeline-jobs/core/src/test/scala/org/swasth/spec/TestMapStreamFunc.scala
+++ hcx-pipeline-jobs/core/src/test/scala/org/swasth/spec/TestMapStreamFunc.scala
@@ -1,25 +1,24 @@
 package org.swasth.spec
 
-import java.util
-
 import org.apache.flink.api.common.typeinfo.TypeInformation
 import org.apache.flink.streaming.api.functions.ProcessFunction
 import org.swasth.dp.core.job.{BaseProcessFunction, Metrics}
+import org.swasth.dp.core.util.Constants
+
+import scala.collection.mutable
 
 
 class TestMapStreamFunc(config: BaseProcessTestConfig)(implicit val stringTypeInfo: TypeInformation[String])
-  extends BaseProcessFunction[util.Map[String, AnyRef], util.Map[String, AnyRef]](config) {
+  extends BaseProcessFunction[mutable.Map[String, AnyRef], mutable.Map[String, AnyRef]](config) {
 
   override def metricsList(): List[String] = {
     List(config.mapEventCount)
   }
 
-  override def processElement(event: util.Map[String, AnyRef],
-                              context: ProcessFunction[util.Map[String, AnyRef], util.Map[String, AnyRef]]#Context,
-                              metrics: Metrics): Unit = {
+  override def processElement(event: mutable.Map[String, AnyRef], context: ProcessFunction[mutable.Map[String, AnyRef], mutable.Map[String, AnyRef]]#Context, metrics: Metrics): Unit = {
     metrics.get(config.mapEventCount)
     metrics.reset(config.mapEventCount)
     metrics.incCounter(config.mapEventCount)
-    context.output(config.mapOutputTag, event)
+    context.output(config.mapOutputTag,  mutable.Map(Constants.TOPIC -> config.kafkaMapOutputTopic, Constants.MESSAGE -> event))
   }
 }
