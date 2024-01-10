b177fcda3538fe4b832ebc01f1d100b28f1ff67b
KrutikaPhirangi <138781661+KrutikaPhirangi@users.noreply.github.com>
2024-01-10 10:45:41 +0530

    Update

--- api-gateway/src/test/java/org/swasth/apigateway/BaseSpec.java
+++ api-gateway/src/test/java/org/swasth/apigateway/BaseSpec.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
