892cc86df0588015cba33abf0480d09b0a537226
KrutikaPhirangi <138781661+KrutikaPhirangi@users.noreply.github.com>
2024-01-23 12:22:04 +0530

    HCX-943:Flink version upgrade from v1.12.0 to 1.15.2 and HCX-948 : Fix Vulnerabilities in hcx-pipelinejobs Module (#598)
    * flink version update
    
    * Update
    
    * Update docker file
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * update
    
    * update
    
    * Update hcx-pipeline-docker-build.yml
    
    * update
    
    * Update
    
    * Update BaseProcessTestConfig.scala
    
    * Update
    
    * Update
    
    * Update BaseProcessTestConfig.scala
    
    * Update BaseProcessTestConfig.scala
    
    * Update BaseProcessTestConfig.scala
    
    * Update BaseProcessTestConfig.scala
    
    * update
    
    * Update
    
    * Update changes in NotificationUtil.scala
    
    * update BaseProcessFunction.scala and NotificationUtil.scala
    
    * update
    
    * Update pom.xml
    
    * Update Dockerfile
    
    * Update NotificationStreamTask.java
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update pom.xml
    
    * update on path
    
    * Update pom.xml
    
    * update
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update pom.xml
    
    * Update
    
    * Update pom.xml
    
    * update
    
    * Update
    
    * commented changes
    
    * update
    
    * Update BaseNotificationFunction.java

--- hcx-pipeline-jobs/coverage-eligibility-job/src/main/java/org/swasth/dp/coverageeligiblity/task/CoverageEligibilityStreamTask.java
+++ hcx-pipeline-jobs/coverage-eligibility-job/src/main/java/org/swasth/dp/coverageeligiblity/task/CoverageEligibilityStreamTask.java
@@ -2,11 +2,12 @@ package org.swasth.dp.coverageeligiblity.task;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.java.typeutils.TypeExtractor;
 import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.swasth.dp.core.function.ContextEnrichmentFunction;
 import org.swasth.dp.core.job.BaseJobConfig;
 import org.swasth.dp.core.job.FlinkKafkaConnector;
@@ -44,9 +45,9 @@ public class CoverageEligibilityStreamTask {
 
     public void process(BaseJobConfig baseJobConfig) throws Exception {
         StreamExecutionEnvironment env = FlinkUtil.getExecutionContext(baseJobConfig);
-        SourceFunction<Map<String,Object>> kafkaConsumer = kafkaConnector.kafkaMapSource(config.kafkaInputTopic);
+        KafkaSource kafkaConsumer = kafkaConnector.kafkaMapSource(config.kafkaInputTopic);
 
-        SingleOutputStreamOperator<Map<String,Object>> enrichedStream = env.addSource(kafkaConsumer, config.coverageEligibilityConsumer)
+        SingleOutputStreamOperator<Map<String,Object>> enrichedStream = env.fromSource(kafkaConsumer, WatermarkStrategy.noWatermarks(), config.coverageEligibilityConsumer)
                 .uid(config.coverageEligibilityConsumer).setParallelism(config.consumerParallelism)
                 .rebalance()
                 .process(new ContextEnrichmentFunction(config, TypeExtractor.getForClass(String.class))).setParallelism(config.downstreamOperatorsParallelism);
@@ -55,7 +56,7 @@ public class CoverageEligibilityStreamTask {
                 .process(new CoverageEligibilityProcessFunction(config)).setParallelism(config.downstreamOperatorsParallelism);
 
         /** Sink for audit events */
-        eventStream.getSideOutput(config.auditOutputTag()).addSink(kafkaConnector.kafkaStringSink(config.auditTopic()))
+        eventStream.getSideOutput(config.auditOutputTag()).sinkTo(kafkaConnector.kafkaStringSink(config.auditTopic()))
                 .name(config.auditProducer()).uid(config.auditProducer()).setParallelism(config.downstreamOperatorsParallelism);
 
         System.out.println(config.jobName() + " is processing");
