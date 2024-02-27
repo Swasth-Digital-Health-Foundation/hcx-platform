6d8b78b6ff574542e2bb1af03acd58941bd2953e
saurabh-singh-17 <saurabhvikashsingh@gmail.com>
2024-02-27 09:07:51 +0530

    ssp added as basepath

--- self-service-portal/src/main.tsx
+++ self-service-portal/src/main.tsx
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
     <ToastContainer></ToastContainer>
-    <Router> 
+    <Router basename='/ssp'> 
       <App />
     </Router>
     </Provider>
