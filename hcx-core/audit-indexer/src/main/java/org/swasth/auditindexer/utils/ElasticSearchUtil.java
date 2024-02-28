09e27c71815a21d22a60f69d8ee0ecacf574c192
KrutikaPhirangi <138781661+KrutikaPhirangi@users.noreply.github.com>
2024-02-28 16:37:13 +0530

    refactor : pom - upgrade elasticsearch version to 7.16.1

--- hcx-core/audit-indexer/src/main/java/org/swasth/auditindexer/utils/ElasticSearchUtil.java
+++ hcx-core/audit-indexer/src/main/java/org/swasth/auditindexer/utils/ElasticSearchUtil.java
@@ -11,7 +11,7 @@ import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.GetIndexRequest;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.xcontent.XContentType;
 
 import java.io.IOException;
 import java.util.Map;
