0f3388bf5fa95244020fd29b4a8862d9a52ec138
Abhishek Gaddi <110591073+AbhiGaddi@users.noreply.github.com>
2024-03-12 19:08:24 +0530

    HCX-990 : Configure notifications for revoked certificates (#611)
    * Feat : Add certificate validations to the hcx-core
    
    * Feat : Add test case
    
    * Feat : ignore for invalid certificates
    
    * Feat : Separate event generation for invalid certificate
    
    * Feat : update the status to Inactive for revoked certificates
    
    * Feat : Add common schedular test case
    
    * Update CertificateRevocationScheduler.java
    
    * Update hcx-apis/src/main/resources/application.yml
    
    Co-authored-by: Mahesh Kumar Gangula <mahesh@sanketika.in>
    
    * Update hcx-apis/src/main/java/org/swasth/hcx/service/ParticipantService.java
    
    Co-authored-by: Mahesh Kumar Gangula <mahesh@sanketika.in>
    
    * Fix : Feedback changes
    
    * Fix : changed return type of updating status
    
    * Fix : Update the feedback changes
    
    * Update CertificateRevocationScheduler.java
    
    * Update CertificateRevocationScheduler.java
    
    * Update CertificateRevocationScheduler.java
    
    * Update CertificateRevocationScheduler.java
    
    * Fix : Changed the logic
    
    * Update CertificateRevocationScheduler.java
    
    * Update CertificateRevocation.java
    
    * Update CertificateRevocation.java
    
    * Update CertificateRevocation.java
    
    * Update OnboardService.java
    
    * Fix : Add test case for invalid scenarios
    
    * Update CertificateRevocation.java
    
    * Feat : Add test case for  registry service
    
    ---------
    
    Co-authored-by: Mahesh Kumar Gangula <mahesh@sanketika.in>

--- hcx-core/hcx-common/src/main/java/org/swasth/common/service/RegistryService.java
+++ hcx-core/hcx-common/src/main/java/org/swasth/common/service/RegistryService.java
@@ -1,5 +1,6 @@
 package org.swasth.common.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import kong.unirest.HttpResponse;
 import kong.unirest.UnirestException;
 import org.swasth.common.exception.ErrorCodes;
@@ -7,7 +8,6 @@ import org.swasth.common.exception.ServerException;
 import org.swasth.common.utils.HttpUtils;
 import org.swasth.common.utils.JSONUtils;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,4 +37,16 @@ public class RegistryService {
         return details;
     }
 
+    public boolean updateStatusOnCertificateRevocation(String osid) throws JsonProcessingException, ServerException {
+        String url = registryUrl + "/api/v1/Organisation/" + osid;
+        Map<String, Object> requestBody = new HashMap<>();
+        requestBody.put("status", "Inactive");
+        HttpResponse<String> response;
+        try {
+            response = HttpUtils.put(url, JSONUtils.serialize(requestBody), new HashMap<>());
+        } catch (UnirestException e) {
+            throw new ServerException(ErrorCodes.ERR_SERVICE_UNAVAILABLE, "Error connecting to registry service: " + e.getMessage());
+        }
+        return response.getStatus() == 200;
+    }
 }
