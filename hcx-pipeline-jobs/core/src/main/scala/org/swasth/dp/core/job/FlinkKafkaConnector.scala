0e0e9105ff6888539f4a5e05f4a70795d2271c81
KrutikaPhirangi <138781661+KrutikaPhirangi@users.noreply.github.com>
2024-06-11 14:26:13 +0530

    Revert "Update"
    This reverts commit 65b177572c9148ec41d2bc2b07033f1ee996465b.

--- hcx-pipeline-jobs/core/src/main/scala/org/swasth/dp/core/job/FlinkKafkaConnector.scala
+++ hcx-pipeline-jobs/core/src/main/scala/org/swasth/dp/core/job/FlinkKafkaConnector.scala
@@ -5,6 +5,7 @@ import org.apache.flink.connector.kafka.sink.KafkaSink
 import org.apache.flink.connector.kafka.source.KafkaSource
 import org.swasth.dp.core.serde._
 
+import java.util._
 class FlinkKafkaConnector(config: BaseJobConfig) extends Serializable {
 
 def kafkaMapSource(kafkaTopic: String): KafkaSource[java.util.Map[String, AnyRef]] = {
@@ -17,7 +18,7 @@ def kafkaMapSource(kafkaTopic: String): KafkaSource[java.util.Map[String, AnyRef
 
   def kafkaMapSink(kafkaTopic: String): KafkaSink[java.util.Map[String, AnyRef]] = {
     KafkaSink.builder[java.util.Map[String, AnyRef]]()
-//      .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+      .setDeliverGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
       .setRecordSerializer(new MapSerializationSchema(kafkaTopic))
       .setKafkaProducerConfig(config.kafkaProducerProperties)
       .build()
@@ -33,7 +34,7 @@ def kafkaMapSource(kafkaTopic: String): KafkaSource[java.util.Map[String, AnyRef
 
   def kafkaStringSink(kafkaTopic: String): KafkaSink[String] = {
     KafkaSink.builder[String]()
-//      .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+      .setDeliverGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
       .setRecordSerializer(new StringSerializationSchema(kafkaTopic))
       .setKafkaProducerConfig(config.kafkaProducerProperties)
       .build()
