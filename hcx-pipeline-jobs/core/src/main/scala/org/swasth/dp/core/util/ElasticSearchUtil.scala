53ecdf183001e9d8d9de9f9b9ffac57bb6a58adb
jagadishkesav <s.jagadishkpd@gmail.com>
2025-04-13 21:24:56 +0530

    Update POM and elastic search utils scala file

--- hcx-pipeline-jobs/core/src/main/scala/org/swasth/dp/core/util/ElasticSearchUtil.scala
+++ hcx-pipeline-jobs/core/src/main/scala/org/swasth/dp/core/util/ElasticSearchUtil.scala
@@ -2,48 +2,47 @@ package org.swasth.dp.core.util
 
 import com.fasterxml.jackson.core.`type`.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
-import co.elastic.clients.elasticsearch.ElasticsearchClient
-import co.elastic.clients.elasticsearch.core.IndexRequest
-import co.elastic.clients.elasticsearch.indices.CreateIndexRequest
-import co.elastic.clients.transport.rest_client.RestClientTransport
-import co.elastic.clients.json.jackson.JacksonJsonpMapper
 import org.apache.commons.lang3.StringUtils
-import org.apache.http.HttpHost
-import org.elasticsearch.client.RestClient
 import org.slf4j.LoggerFactory
-import com.typesafe.config.ConfigFactory
 
-import java.io.IOException
+import co.elastic.clients.elasticsearch.ElasticsearchClient
+import co.elastic.clients.elasticsearch._types.mapping.TypeMapping
+import co.elastic.clients.elasticsearch.indices.{CreateIndexRequest, ExistsRequest}
+import co.elastic.clients.elasticsearch.core.IndexRequest
+import co.elastic.clients.json.jackson.JacksonJsonpMapper
+import co.elastic.clients.transport.endpoints.BooleanResponse
+import co.elastic.clients.transport.rest_client.RestClientTransport
+import org.apache.http.HttpHost
+import org.apache.http.client.config.RequestConfig
+import org.elasticsearch.client.{RestClient, RestClientBuilder}
+
+import java.io.{ByteArrayInputStream, IOException}
 import java.util
-import java.util.function.Function
 
-class ElasticSearchUtil(configPath: String, indexName: String, batchSize: Int = 1000) extends Serializable {
-
-  private val config = ConfigFactory.load(configPath)
-  private val esBasePath = config.getString("es.basePath")
-  private val host = esBasePath.split(":")(0)
-  private val port = esBasePath.split(":")(1).toInt
-  private val esClient: ElasticsearchClient = createClient(host, port)
+class ElasticSearchUtil(connectionInfo: String, indexName: String, batchSize: Int = 1000) extends Serializable {
+  private val resultLimit = 100
+  private val esClient: ElasticsearchClient = createClient(connectionInfo)
   private val mapper = new ObjectMapper
-
   private[this] val logger = LoggerFactory.getLogger(classOf[ElasticSearchUtil])
 
   System.setProperty("es.set.netty.runtime.available.processors", "false")
 
-  private def createClient(host: String, port: Int): ElasticsearchClient = {
-    if (host.isEmpty || host.startsWith("//")) {
-      throw new IllegalArgumentException(s"Invalid host: $host. Please provide a valid hostname.")
-    }
+  private def createClient(connectionInfo: String): ElasticsearchClient = {
+    val httpHosts: List[HttpHost] = connectionInfo.split(",").map(info => {
+      val host = info.split(":")(0)
+      val port = info.split(":")(1).toInt
+      new HttpHost(host, port)
+    }).toList
 
-    val validPort = try {
-      port
-    } catch {
-      case _: NumberFormatException =>
-        throw new IllegalArgumentException(s"Invalid port number: $port. Please provide a valid integer.")
-    }
+    val builder: RestClientBuilder = RestClient.builder(httpHosts: _*).setRequestConfigCallback(
+      new RestClientBuilder.RequestConfigCallback() {
+        override def customizeRequestConfig(requestConfigBuilder: RequestConfig.Builder): RequestConfig.Builder = {
+          requestConfigBuilder.setConnectionRequestTimeout(-1)
+        }
+      }
+    )
 
-    val httpHost = new HttpHost(host, validPort)
-    val restClient = RestClient.builder(httpHost).build()
+    val restClient = builder.build()
     val transport = new RestClientTransport(restClient, new JacksonJsonpMapper())
     new ElasticsearchClient(transport)
   }
@@ -52,16 +51,16 @@ class ElasticSearchUtil(configPath: String, indexName: String, batchSize: Int = 
   def addDocumentWithIndex(document: String, indexName: String, identifier: String): Unit = {
     try {
       val doc = mapper.readValue(document, new TypeReference[util.Map[String, AnyRef]]() {})
-      val indexRequestFunction: java.util.function.Function[
-        co.elastic.clients.elasticsearch.core.IndexRequest.Builder[java.util.Map[String, AnyRef]],
-        co.elastic.clients.util.ObjectBuilder[
-          co.elastic.clients.elasticsearch.core.IndexRequest[java.util.Map[String, AnyRef]]
-        ]
-      ] = builder => builder.index(indexName).id(identifier).document(doc)
+      
+      val request = IndexRequest.of[util.Map[String, AnyRef]](
+        builder => builder
+          .index(indexName)
+          .id(identifier)
+          .document(doc)
+      )
 
-      val indexRequest = IndexRequest.of(indexRequestFunction)
-      esClient.index(indexRequest)
-      logger.info(s"Added $identifier to index $indexName")
+      val response = esClient.index(request)
+      logger.info(s"Added ${response.id()} to index ${response.index()}")
     } catch {
       case e: IOException =>
         logger.error(s"ElasticSearchUtil:: Error while adding document to index : $indexName : " + e.getMessage)
@@ -72,11 +71,29 @@ class ElasticSearchUtil(configPath: String, indexName: String, batchSize: Int = 
 
   def addIndex(settings: String, mappings: String, indexName: String, alias: String): Unit = {
     try {
-      if (!isIndexExists(indexName)) {
-        val createRequest = CreateIndexRequest.of(_.index(indexName)
-          .settings(s => s.withJson(new java.io.ByteArrayInputStream(settings.getBytes)))
-          .mappings(m => m.withJson(new java.io.ByteArrayInputStream(mappings.getBytes))))
+      if(!isIndexExists(indexName)) {
+        var createRequestBuilder = new CreateIndexRequest.Builder()
+          .index(indexName)
+        
+        if (StringUtils.isNotBlank(settings)) {
+          createRequestBuilder = createRequestBuilder.settings(
+            s => s.withJson(new ByteArrayInputStream(settings.getBytes()))
+          )
+        }
+        
+        if (StringUtils.isNotBlank(mappings)) {
+          createRequestBuilder = createRequestBuilder.mappings(
+            m => m.withJson(new ByteArrayInputStream(mappings.getBytes()))
+          )
+        }
+        
+        if (StringUtils.isNotBlank(alias)) {
+          createRequestBuilder = createRequestBuilder.aliases(alias, a => a.isWriteIndex(true))
+        }
+        
+        val createRequest = createRequestBuilder.build()
         esClient.indices().create(createRequest)
+        logger.info(s"Created index: $indexName with alias: $alias")
       }
     } catch {
       case e: Exception =>
@@ -88,19 +105,26 @@ class ElasticSearchUtil(configPath: String, indexName: String, batchSize: Int = 
 
   def isIndexExists(indexName: String): Boolean = {
     try {
-      esClient.indices().exists(_.index(indexName)).value()
+      val request = new ExistsRequest.Builder().index(indexName).build()
+      val response: BooleanResponse = esClient.indices().exists(request)
+      response.value()
     } catch {
-      case e: IOException =>
-        logger.error("ElasticSearchUtil:: Failed to check Index if Present or not. Exception : ", e)
+      case e: IOException => {
+        logger.error("ElasticSearchUtil:: Failed to check Index if Present or not. Exception: ", e)
         false
+      }
     }
   }
 
   def close(): Unit = {
-    try esClient._transport().close()
-    catch {
-      case e: IOException => e.printStackTrace()
+    if (esClient != null) {
+      try {
+        esClient._transport().close()
+      } catch {
+        case e: IOException => 
+          logger.error("Error closing ElasticSearch client", e)
+          e.printStackTrace()
+      }
     }
   }
-
 }
